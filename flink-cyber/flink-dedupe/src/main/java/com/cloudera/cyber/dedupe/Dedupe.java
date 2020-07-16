59200eb757f61537dea499521caa34ee4176669d
Simon Elliston Ball <simon@simonellistonball.com>
2020-07-16 17:52:48 +0100

    Refactor for easier re-use of actions in DeDupe and Sessionizer (for embedding in other pipelines)

---
+++ flink-cyber/flink-dedupe/src/main/java/com/cloudera/cyber/dedupe/Dedupe.java
@@ -0,0 +1,31 @@
+package com.cloudera.cyber.dedupe;
+
+import com.cloudera.cyber.Message;
+import com.cloudera.cyber.dedupe.impl.CreateKeyFromMessage;
+import com.cloudera.cyber.dedupe.impl.EventTimeAndCountTrigger;
+import com.cloudera.cyber.dedupe.impl.SumAndMaxTs;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.OutputTag;
+
+import java.util.List;
+import java.util.Map;
+
+public class Dedupe {
+    public static SingleOutputStreamOperator<DedupeMessage> dedupe(DataStream<Message> source, List<String> sessionKey, Long maxTime, Long maxCount, OutputTag<DedupeMessage> lateData) {
+        return source
+                .map(new CreateKeyFromMessage(sessionKey))
+                .keyBy(new KeySelector<DedupeMessage, Map<String, String>>() {
+                    @Override
+                    public Map<String, String> getKey(DedupeMessage dedupeMessage) throws Exception {
+                        return dedupeMessage.getFields();
+                    }
+                })
+                .timeWindow(Time.milliseconds(maxTime))
+                .sideOutputLateData(lateData)
+                .trigger(EventTimeAndCountTrigger.of(maxCount))
+                .aggregate(new SumAndMaxTs());
+    }
+}
--- flink-cyber/flink-sessions/src/main/java/com/cloudera/cyber/sessions/SessionJob.java
+++ flink-cyber/flink-sessions/src/main/java/com/cloudera/cyber/sessions/SessionJob.java
@@ -2,20 +2,16 @@ package com.cloudera.cyber.sessions;
 
 import com.cloudera.cyber.GroupedMessage;
 import com.cloudera.cyber.Message;
-import com.cloudera.cyber.flink.operators.MessageConcatenate;
-import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+
+import static com.cloudera.cyber.sessions.Session.sessionize;
 
 public abstract class SessionJob {
     private static final long DEFAULT_SESSION_TIMEOUT = 1000;
@@ -35,20 +31,6 @@ public abstract class SessionJob {
         writeResults(params, results);
 
         return env;
-    }
-
-    private SingleOutputStreamOperator<GroupedMessage> sessionize(DataStream<Message> source, final List<String> sessionKey, final Long sessionTimeout) {
-        return source
-                .keyBy(new KeySelector<Message, Map<String, String>>() {
-                    @Override
-                    public Map<String, String> getKey(Message message) throws Exception {
-                        return sessionKey.stream().collect(Collectors.toMap(
-                                f -> f.toString(),
-                                f -> message.get(f).toString()));
-                    }
-                })
-                .window(EventTimeSessionWindows.withGap(Time.milliseconds(sessionTimeout)))
-                .aggregate(new MessageConcatenate());
     }
 
     protected abstract void writeResults(ParameterTool params, SingleOutputStreamOperator<GroupedMessage> results);
