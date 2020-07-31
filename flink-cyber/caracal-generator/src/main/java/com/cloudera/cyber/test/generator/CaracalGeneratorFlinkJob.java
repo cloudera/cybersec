aec112b8da0197e7da080788bedae338d0b50310
Simon Elliston Ball <simon@simonellistonball.com>
2020-07-31 18:24:46 +0100

    Fix to run caracal sample generator on Flink yarn cluster

--- flink-cyber/caracal-generator/src/main/java/com/cloudera/cyber/test/generator/CaracalGeneratorFlinkJob.java
+++ flink-cyber/caracal-generator/src/main/java/com/cloudera/cyber/test/generator/CaracalGeneratorFlinkJob.java
@@ -8,28 +8,14 @@ import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
-import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
-import org.apache.kafka.clients.producer.ProducerRecord;
 
-import javax.annotation.Nullable;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CaracalGeneratorFlinkJob {
+public abstract class CaracalGeneratorFlinkJob {
     public static final String PARAMS_RECORDS_LIMIT = "generator.count";
 
-    public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            throw new RuntimeException("Path to the properties file is expected as the only argument.");
-        }
-        ParameterTool params = ParameterTool.fromPropertiesFile(args[0]);
-
-        new CaracalGeneratorFlinkJob().run(params);
-    }
-
-    public void run(ParameterTool params) throws Exception {
+    public StreamExecutionEnvironment createPipeline(ParameterTool params) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
@@ -48,54 +34,26 @@ public class CaracalGeneratorFlinkJob {
         SingleOutputStreamOperator<Tuple2<String, String>> generatedInput =
                 env.addSource(new FreemarkerTemplateSource(outputs, params.getLong(PARAMS_RECORDS_LIMIT, -1))).name("Weighted Data Source");
 
-        FlinkKafkaProducer<Tuple2<String, String>> kafkaSink = new FlinkKafkaProducer<Tuple2<String, String>>(
-                "generator.output",
-                new KafkaSerializationSchema<Tuple2<String, String>>() {
-                    @Override
-                    public ProducerRecord<byte[], byte[]> serialize(Tuple2<String, String> stringStringTuple2, @Nullable Long aLong) {
-                        return new ProducerRecord<byte[],byte[]>(
-                                stringStringTuple2.f0,
-                                stringStringTuple2.f1.getBytes(StandardCharsets.UTF_8)
-                        );
-                    }
-                },
-                Utils.readKafkaProperties(params, false),
-                FlinkKafkaProducer.Semantic.AT_LEAST_ONCE);
-
-
-        FlinkKafkaProducer<Tuple2<String, Integer>> metricsSink = new FlinkKafkaProducer<Tuple2<String, Integer>>(
-                "generator.metrics",
-                new KafkaSerializationSchema<Tuple2<String, Integer>>() {
-                    @Override
-                    public ProducerRecord<byte[], byte[]> serialize(Tuple2<String, Integer> stringStringTuple2, @Nullable Long timestamp) {
-                        return new ProducerRecord<byte[],byte[]>(
-                                "generator.metrics",
-                                null,
-                                timestamp,
-                                stringStringTuple2.f0.getBytes(StandardCharsets.UTF_8),
-                                stringStringTuple2.f1.toString().getBytes(StandardCharsets.UTF_8)
-                        );
-                    }
-                },
-                Utils.readKafkaProperties(params, false),
-                FlinkKafkaProducer.Semantic.AT_LEAST_ONCE);
-        
-
         SingleOutputStreamOperator<Tuple2<String, Integer>> metrics = generatedInput
                 .map(new MapFunction<Tuple2<String, String>, Tuple2<String, Integer>>() {
-                        @Override
-                        public Tuple2<String, Integer> map(Tuple2<String, String> stringStringTuple2) throws Exception {
-                            return Tuple2.of(stringStringTuple2.f0, Integer.valueOf(1));
-                        }
-                    })
+                    @Override
+                    public Tuple2<String, Integer> map(Tuple2<String, String> stringStringTuple2) throws Exception {
+                        return Tuple2.of(stringStringTuple2.f0, Integer.valueOf(1));
+                    }
+                })
                 .keyBy(0)
                 .window(TumblingProcessingTimeWindows.of(Time.seconds(30)))
                 .sum(1);
-        metrics.addSink(metricsSink);
 
-        generatedInput.addSink(kafkaSink).name("Kafka Sink");
+        writeMetrics(params, metrics);
+        writeResults(params, generatedInput);
 
-        env.execute("Caracal Data generator");
+        return env;
     }
+
+    protected abstract void writeMetrics(ParameterTool params, SingleOutputStreamOperator<Tuple2<String, Integer>> metrics);
+
+    protected abstract void writeResults(ParameterTool params, SingleOutputStreamOperator<Tuple2<String, String>> generatedInput);
+
 
 }
