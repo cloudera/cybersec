cdb42b1851d1ec80ec1e77a68f26ab4c792c17fc
Simon Elliston Ball <simon@simonellistonball.com>
2020-08-13 15:28:28 +0100

    Avro changes and MessageUtils

---
+++ flink-cyber/flink-cyber-api/src/test/java/com/cloudera/cyber/TypeTests.java
@@ -0,0 +1,21 @@
+package com.cloudera.cyber;
+
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.codehaus.jackson.type.TypeReference;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class TypeTests {
+
+    @Test
+    public void testTypes() {
+        TypeInformation<EnrichmentEntry> types = TypeInformation.of(EnrichmentEntry.class);
+
+        TypeInformation<Map<String, String>> mapType = TypeInformation.of(new TypeHint<Map<String, String>>() {
+        });
+
+        System.out.println(types);
+    }
+}
