fb9307b00f2265d89b33c876c426f1e7cc5e5c76
Simon Elliston Ball <simon@simonellistonball.com>
2020-08-15 03:57:37 +0100

    Made message additions work with immutables

--- flink-cyber/flink-common/src/main/java/com/cloudera/cyber/MessageUtils.java
+++ flink-cyber/flink-common/src/main/java/com/cloudera/cyber/MessageUtils.java
@@ -3,10 +3,16 @@ package com.cloudera.cyber;
 import com.cloudera.cyber.Message;
 
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MessageUtils {
     public static Message addFields(Message message, Map<String, String> field) {
-        message.getExtensions().putAll(field);
-        return message;
+        return Message.newBuilder(message)
+                .setExtensions(Stream.concat(
+                        message.getExtensions().entrySet().stream(),
+                        field.entrySet().stream()
+                ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)))
+                .build();
     }
 }
