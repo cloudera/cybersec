f7786185b14972dc85e47efb18d108393f4eec45
Simon Elliston Ball <simon@simonellistonball.com>
2020-08-15 04:25:23 +0100

    Added Solr Indexing stub and some cleanup

--- flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexer/SolrJobKafka.java
+++ flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexer/SolrJobKafka.java
@@ -1,37 +1,31 @@
 package com.cloudera.cyber.indexer;
 
-import com.cloudera.cyber.flink.FlinkUtils;
 import com.cloudera.cyber.Message;
+import com.cloudera.cyber.flink.FlinkUtils;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.core.fs.Path;
-import org.apache.flink.formats.parquet.avro.ParquetAvroWriters;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
 import org.apache.flink.util.Preconditions;
 
 import static com.cloudera.cyber.flink.ConfigConstants.PARAMS_TOPIC_INPUT;
 
-public class ParquetJobKafka extends ParquetJob {
+public class SolrJobKafka extends SolrJob {
 
     public static void main(String[] args) throws Exception {
         Preconditions.checkArgument(args.length == 1, "Arguments must consist of a single properties file");
-        new ParquetJobKafka().createPipeline(ParameterTool.fromArgs(args)).execute("Indexing - Parquet");
+        new SolrJobKafka().createPipeline(ParameterTool.fromArgs(args)).execute("Indexing - Solr");
     }
+
 
     @Override
     public DataStream<Message> createSource(StreamExecutionEnvironment env, ParameterTool params) {
         return env.addSource(
-                new FlinkUtils(Message.class).createKafkaSource(params.getRequired(PARAMS_TOPIC_INPUT), params, "indexer-parquet")
+                new FlinkUtils(Message.class).createKafkaSource(params.getRequired(PARAMS_TOPIC_INPUT), params, "indexer-solr")
         );
     }
 
     @Override
     protected void writeResults(DataStream<Message> results, ParameterTool params) {
-        final StreamingFileSink<Message> sink = StreamingFileSink
-                .forBulkFormat(new Path(params.getRequired("output.path")),
-                        ParquetAvroWriters.forSpecificRecord(Message.class))
-                .build();
-        results.addSink(sink);
+        throw new IllegalArgumentException("Solr not yet implemented");
     }
 }
