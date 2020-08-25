0551f1264a821c2bc0bd3252644cb0d76782fb25
Simon Elliston Ball <simon@simonellistonball.com>
2020-08-25 15:24:18 +0100

    Refactored Lookup Raw job to support easier composition and better state naming

--- flink-cyber/flink-enrichment/flink-enrichment-lookup-raw/src/main/java/com/cloudera/cyber/enrichment/lookup/LookupJob.java
+++ flink-cyber/flink-enrichment/flink-enrichment-lookup-raw/src/main/java/com/cloudera/cyber/enrichment/lookup/LookupJob.java
@@ -2,12 +2,12 @@ package com.cloudera.cyber.enrichment.lookup;
 
 import com.cloudera.cyber.EnrichmentEntry;
 import com.cloudera.cyber.Message;
-import com.cloudera.cyber.MessageUtils;
+import com.cloudera.cyber.enrichment.lookup.config.EnrichmentConfig;
 import com.cloudera.cyber.enrichment.lookup.config.EnrichmentKind;
 import com.cloudera.cyber.flink.CyberJob;
+import com.cloudera.cyber.flink.FlinkUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.state.MapStateDescriptor;
-import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -16,12 +16,9 @@ import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.BroadcastStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
-import org.apache.flink.util.Collector;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -32,27 +29,24 @@ import static com.cloudera.cyber.enrichment.ConfigUtils.*;
 @Slf4j
 public abstract class LookupJob implements CyberJob {
 
-    @Override
-    public StreamExecutionEnvironment createPipeline(ParameterTool params) throws Exception {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+    public static DataStream<Message> enrich(DataStream<EnrichmentEntry> baseEnrichmentSource,
+                                         DataStream<Message> source,
+                                         List<EnrichmentConfig> configs
+    ) {
+        DataStream<EnrichmentEntry> enrichmentSource = baseEnrichmentSource.keyBy(e -> EnrichmentKey.builder()
+                .type(e.getType())
+                .key(e.getKey())
+                .build()
+        );
 
-        DataStream<Message> source = createSource(env, params);
-        DataStream<EnrichmentEntry> enrichmentSource = createEnrichmentSource(env, params)
-                .keyBy(e -> EnrichmentKey.builder()
-                        .type(e.getType())
-                        .key(e.getKey())
-                        .build()
-                );
-
-        byte[] configJson = Files.readAllBytes(Paths.get(params.getRequired(PARAMS_CONFIG_FILE)));
-        Map<String, List<String>> typeToFields = typeToFields(allConfigs(configJson), EnrichmentKind.LOCAL);
-        Set<String> enrichmentTypes = enrichmentTypes(allConfigs(configJson), EnrichmentKind.LOCAL);
+        Map<String, List<String>> typeToFields = typeToFields(configs, EnrichmentKind.LOCAL);
+        Set<String> enrichmentTypes = enrichmentTypes(configs, EnrichmentKind.LOCAL);
 
         Map<String, MapStateDescriptor<String, Map<String, String>>> broadcastDescriptors = enrichmentTypes.stream().collect(Collectors.toMap(
                 v -> v,
                 enrichmentType -> new MapStateDescriptor<String, Map<String, String>>(enrichmentType, Types.STRING, Types.MAP(Types.STRING, Types.STRING)))
         );
+
         Map<String, BroadcastStream<EnrichmentEntry>> enrichmentBroadcasts = enrichmentTypes.stream()
                 .map(enrichmentType ->
                         Tuple2.of(enrichmentType, enrichmentSource.filter(f -> f.getType().equals(enrichmentType))
@@ -71,39 +65,24 @@ public abstract class LookupJob implements CyberJob {
                     List<String> fields = entry.getValue();
                     String type = entry.getKey();
                     return m.connect(enrichmentBroadcasts.get(type))
-                            .process(new BroadcastProcessFunction<Message, EnrichmentEntry, Message>() {
-                                @Override
-                                public void processElement(Message message, ReadOnlyContext readOnlyContext, Collector<Message> collector) throws Exception {
-                                    ReadOnlyBroadcastState<String, Map<String, String>> bc = readOnlyContext.getBroadcastState(broadcastDescriptors.get(type));
-                                    if (bc != null) {
-                                        HashMap<String, String> hm = new HashMap<>();
-                                        for (String field : fields) {
-                                            Object value = message.getExtensions().get(field);
-                                            if (value != null && bc.contains(value.toString())) {
-                                                hm.putAll(bc.get(value.toString()).entrySet().stream().collect(Collectors.toMap(
-                                                        k -> field + "_" + k.getKey(),
-                                                        v -> v.getValue()
-                                                )));
-                                            }
-                                        }
-                                        collector.collect(MessageUtils.addFields(message, hm));
-                                    } else {
-                                        log.warn("Failed to find broadcast lookup for %s", type);
-                                    }
-                                }
-
-                                @Override
-                                public void processBroadcastElement(EnrichmentEntry enrichmentEntry, Context context, Collector<Message> collector) throws Exception {
-                                    // add to the state
-                                    context.getBroadcastState(broadcastDescriptors.get(enrichmentEntry.getType()))
-                                            .put(
-                                                    enrichmentEntry.getKey(),
-                                                    enrichmentEntry.getEntries()
-                                            );
-                                }
-                            }).name("BroadcastProcess: " + type).uid("broadcast-process-" + type);
+                            .process(new EnrichmentBroadcastProcessFunction(type, fields, broadcastDescriptors)).name("BroadcastProcess: " + type).uid("broadcast-process-" + type);
                 }, (a, b) -> a); // TODO - does the combiner really make sense?
 
+        return pipeline;
+    }
+
+    @Override
+    public StreamExecutionEnvironment createPipeline(ParameterTool params) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        FlinkUtils.setupEnv(env, params);
+
+        DataStream<Message> source = createSource(env, params);
+        DataStream<EnrichmentEntry> enrichmentSource = createEnrichmentSource(env, params);
+
+        byte[] configJson = Files.readAllBytes(Paths.get(params.getRequired(PARAMS_CONFIG_FILE)));
+
+        DataStream<Message> pipeline = enrich(enrichmentSource, source, allConfigs(configJson));
         writeResults(env, params, pipeline);
         return env;
     }
@@ -115,7 +94,6 @@ public abstract class LookupJob implements CyberJob {
     protected abstract DataStream<EnrichmentEntry> createEnrichmentSource(StreamExecutionEnvironment env, ParameterTool params);
 
     public static class Descriptors {
-
         public static MapStateDescriptor<EnrichmentKey, Map<String, String>> broadcast = new MapStateDescriptor<>("enrichment",
                 TypeInformation.of(EnrichmentKey.class),
                 Types.MAP(Types.STRING, Types.STRING));
