704bc3b1311c982dd77e5106d0b5dca460bc9049
Simon Elliston Ball <simon@simonellistonball.com>
2020-08-25 15:27:32 +0100

    Added support for fallback formats in TimestampFormatParser

--- flink-cyber/metron-parser-chain/parser-chains-parsers/src/main/java/com/cloudera/parserchains/parsers/TimestampFormatParser.java
+++ flink-cyber/metron-parser-chain/parser-chains-parsers/src/main/java/com/cloudera/parserchains/parsers/TimestampFormatParser.java
@@ -13,20 +13,21 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.TemporalAccessor;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TimeZone;
+import java.util.*;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * A parser to extract formatted timestamps and express them as epoch time, preserving the
  * timezone of the original in a complementary field
  */
 @MessageParser(
-        name="TimestampFormat",
-        description="Parse a formatted timestamp into usable unix epoch time")
-public class TimestampFormatParser  implements Parser {
-    private static final String DEFAULT_TIMEFORMAT = "yyyyMMddThh:mm:ss.sssZ";
+        name = "TimestampFormat",
+        description = "Parse a formatted timestamp into usable unix epoch time")
+public class TimestampFormatParser implements Parser {
+    private final List<String> DEFAULT_TIMEFORMAT = Arrays.asList("yyyyMMddThh:mm:ss.sssZ");
     private static final String DEFAULT_TIMEZONE = TimeZone.getDefault().getID();
     private List<Config> fields = new ArrayList<>();
 
@@ -34,11 +35,22 @@ public class TimestampFormatParser  implements Parser {
     public Message parse(Message input) {
         Message.Builder builder = Message.builder()
                 .withFields(input);
-        for(Config c: fields) {
+        for (Config c : fields) {
             FieldName fieldName = FieldName.of(c.fieldName);
             String inputValue = input.getField(fieldName).get().get();
-            FieldValue value = FieldValue.of(parseDate(inputValue, c.dateTimeFormatter, c.tz).toString());
-            builder.addField(fieldName, value);
+            // try all the date formatters until one works
+            Iterator<DateTimeFormatter> i = c.dateTimeFormatter.listIterator();
+            while (i.hasNext()) {
+                DateTimeFormatter formatter = i.next();
+                try {
+                    FieldValue value = FieldValue.of(parseDate(inputValue, formatter, c.tz).toString());
+                    builder.addField(fieldName, value);
+                    break;
+                } catch (DateTimeParseException e) {
+                    // if this is the last one, throw it.
+                    if (!i.hasNext()) throw (e);
+                }
+            }
         }
         return builder.build();
     }
@@ -48,17 +60,23 @@ public class TimestampFormatParser  implements Parser {
         return Instant.from(parse).toEpochMilli();
     }
 
-    @Configurable(key="fields",
-            label="Time Fields",
-            description="The field that will contain the timestamp.")
+    @Configurable(key = "fields",
+            label = "Time Fields",
+            description = "The field that will contain the timestamp.")
     public TimestampFormatParser withOutputField(
-            @Parameter(key="field", label="Input Field", description = "Field to be parsed", required = true) String fieldName,
-            @Parameter(key="format", label="Time format", description = "A compatible time format", required = true, defaultValue = DEFAULT_TIMEFORMAT) String format,
-            @Parameter(key="tz", label="Timezome", description = "Optionally set the expected timezone", required = true) String tz
+            @Parameter(key = "field", label = "Input Field", description = "Field to be parsed", required = true) String fieldName,
+            @Parameter(key = "format", label = "Time format", description = "A compatible time format", required = true) String format,
+            @Parameter(key = "tz", label = "Timezome", description = "Optionally set the expected timezone", required = true) String tz
     ) {
-        if(StringUtils.isNotBlank(fieldName)) {
+
+        List<String> formats = Arrays.asList(format.split(","));
+
+        if (formats == null)
+            formats = DEFAULT_TIMEFORMAT;
+
+        if (StringUtils.isNotBlank(fieldName)) {
             this.fields.add(new TimestampFormatParser.Config(fieldName,
-                    DateTimeFormatter.ofPattern(format),
+                    formats.stream().map(f -> DateTimeFormatter.ofPattern(f)).collect(toList()),
                     StringUtils.isNotBlank(tz) ? tz : DEFAULT_TIMEZONE));
         }
         return this;
@@ -66,9 +84,10 @@ public class TimestampFormatParser  implements Parser {
 
     public class Config implements Serializable {
         private String fieldName;
-        private DateTimeFormatter dateTimeFormatter;
+        protected List<DateTimeFormatter> dateTimeFormatter;
         private String tz;
-        public Config(String fieldName, DateTimeFormatter dateTimeFormatter, String tz) {
+
+        public Config(String fieldName, List<DateTimeFormatter> dateTimeFormatter, String tz) {
             this.fieldName = fieldName;
             this.dateTimeFormatter = dateTimeFormatter;
             this.tz = tz;
