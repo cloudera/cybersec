0aa8d228718245747c9dd94cd17b868571c03486
Simon Elliston Ball <simon@simonellistonball.com>
2020-08-25 15:22:03 +0100

    Initial simple indexing to Parquet format on HDFS

--- flink-cyber/flink-indexing/flink-indexing-parquet/src/main/java/com/cloudera/cyber/indexer/ParquetJob.java
+++ flink-cyber/flink-indexing/flink-indexing-parquet/src/main/java/com/cloudera/cyber/indexer/ParquetJob.java
@@ -1,6 +1,7 @@
 package com.cloudera.cyber.indexer;
 
 import com.cloudera.cyber.Message;
+import com.cloudera.cyber.flink.FlinkUtils;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -11,10 +12,17 @@ public abstract class ParquetJob {
     protected StreamExecutionEnvironment createPipeline(ParameterTool params) {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        FlinkUtils.setupEnv(env, params);
 
         DataStream<Message> source = createSource(env, params);
 
         // TODO - apply any filtering and index projection logic here
+
+        // now add the correctly formed version for tables
+
+        // this will read an expected schema and map the incoming messages by type to the expected output fields
+        // unknown fields will either be dumped in a 'custom' map field or dropped based on config
+
 
         writeResults(source, params);
 
@@ -22,4 +30,5 @@ public abstract class ParquetJob {
     }
     protected abstract DataStream<Message> createSource(StreamExecutionEnvironment env, ParameterTool params);
     protected abstract void writeResults(DataStream<Message> results, ParameterTool params);
+    protected abstract void writeTabularResults(DataStream<Message> results, String path, ParameterTool params);
 }
--- flink-cyber/flink-indexing/flink-indexing-parquet/src/main/java/com/cloudera/cyber/indexer/ParquetJobKafka.java
+++ flink-cyber/flink-indexing/flink-indexing-parquet/src/main/java/com/cloudera/cyber/indexer/ParquetJobKafka.java
@@ -14,24 +14,26 @@ import static com.cloudera.cyber.flink.ConfigConstants.PARAMS_TOPIC_INPUT;
 
 public class ParquetJobKafka extends ParquetJob {
 
+    private static final String PARAMS_OUTPUT_BASEPATH = "index.basepath";
+
     public static void main(String[] args) throws Exception {
         Preconditions.checkArgument(args.length == 1, "Arguments must consist of a single properties file");
-        new ParquetJobKafka().createPipeline(ParameterTool.fromArgs(args)).execute("Indexing - Parquet");
+        new ParquetJobKafka().createPipeline(ParameterTool.fromPropertiesFile(args[0])).execute("Indexing - Parquet");
     }
 
     @Override
     public DataStream<Message> createSource(StreamExecutionEnvironment env, ParameterTool params) {
         return env.addSource(
                 new FlinkUtils(Message.class).createKafkaSource(params.getRequired(PARAMS_TOPIC_INPUT), params, "indexer-parquet")
-        );
+        ).name("Kafka Source").uid("kafka-source");
     }
 
     @Override
     protected void writeResults(DataStream<Message> results, ParameterTool params) {
         final StreamingFileSink<Message> sink = StreamingFileSink
-                .forBulkFormat(new Path(params.getRequired("output.path")),
+                .forBulkFormat(new Path(params.getRequired(PARAMS_OUTPUT_BASEPATH)),
                         ParquetAvroWriters.forSpecificRecord(Message.class))
                 .build();
-        results.addSink(sink);
+        results.addSink(sink).name("Parquet Sink").uid("parquet-sink");
     }
 }
