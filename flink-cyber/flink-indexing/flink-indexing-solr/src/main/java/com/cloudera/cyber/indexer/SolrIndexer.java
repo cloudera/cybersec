cea9ad7d8cc044d303536cf5957d7b1e85910a03
Simon Elliston Ball <simon@simonellistonball.com>
2020-08-27 13:57:14 +0100

    Initial solr implementation of indexing

--- flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexer/SolrIndexer.java
+++ flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexer/SolrIndexer.java
@@ -1,4 +1,92 @@
 package com.cloudera.cyber.indexer;
 
-public class SolrIndexer {
+import com.cloudera.cyber.indexing.IndexEntry;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.windowing.RichWindowFunction;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.UpdateResponse;
+import org.apache.solr.common.SolrInputDocument;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.groupingBy;
+
+@Slf4j
+public class SolrIndexer extends RichWindowFunction<IndexEntry, UpdateResponse, String, TimeWindow> {
+
+    public static final String SOLR_URLS_KEY = "solr.urls";
+    public static final String SOLR_COLLECTION_KEY = "solr.collection";
+
+    public static final String SOLR_SSL_LOCATION_KEY = "solr.ssl.truststore.location";
+    public static final String SOLR_SSL_PWD_KEY = "solr.ssl.truststore.password";
+
+    private final List<String> solrUrls;
+    private final String solrCollection;
+    private final String trustStorePath;
+    private final String trustStorePassword;
+
+    private transient SolrClient solrClient;
+
+    public SolrIndexer(ParameterTool params) {
+        solrUrls = Lists.newArrayList(params.getRequired(SOLR_URLS_KEY).split(","));
+        solrCollection = params.getRequired(SOLR_COLLECTION_KEY);
+        trustStorePath = params.get(SOLR_SSL_LOCATION_KEY);
+        trustStorePassword = params.get(SOLR_SSL_PWD_KEY);
+    }
+
+    @Override
+    public void apply(String k, TimeWindow w, Iterable<IndexEntry> logs, Collector<UpdateResponse> output) throws Exception {
+        Map<String, List<IndexEntry>> collect = StreamSupport.stream(logs.spliterator(), false).collect(groupingBy(IndexEntry::getIndex));
+        // TODO - account for all the errors, not just the last
+        AtomicReference<Exception> lastError = new AtomicReference<>();
+        collect.forEach((collection, entries) -> {
+            try {
+                output.collect(solrClient.add(collection, mapToSolrDocuments(entries)));
+            } catch (SolrServerException | IOException e) {
+                log.error("Solr Exception", e);
+                lastError.set(e);
+            }
+        });
+        if (lastError.get() != null) {
+            throw(lastError.get());
+        }
+    }
+
+    private List<SolrInputDocument> mapToSolrDocuments(Iterable<IndexEntry> logs) {
+        List<SolrInputDocument> docs = new ArrayList<>();
+        for (IndexEntry entry : logs) {
+            SolrInputDocument doc = new SolrInputDocument();
+            entry.getFields().forEach(doc::addField);
+            doc.addField("timestamp", entry.getTimestamp());
+            doc.addField("id", entry.getId());
+            docs.add(doc);
+        }
+        return docs;
+    }
+
+    @Override
+    public void open(Configuration config) {
+        solrClient = SolrClientBuilder.builder()
+                .solrUrls(solrUrls)
+                .build().build();
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (solrClient != null) {
+            solrClient.close();
+            solrClient = null;
+        }
+    }
 }
--- flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexer/SolrJob.java
+++ flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexer/SolrJob.java
@@ -1,24 +1,65 @@
 package com.cloudera.cyber.indexer;
 
 import com.cloudera.cyber.Message;
+import com.cloudera.cyber.flink.EventTimeAndCountTrigger;
+import com.cloudera.cyber.indexing.IndexEntry;
+import com.cloudera.cyber.indexing.SearchIndexJob;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.request.CollectionAdminRequest;
+import org.apache.solr.client.solrj.request.schema.SchemaRequest;
+import org.apache.solr.client.solrj.response.schema.SchemaResponse;
 
-public abstract class SolrJob {
-    protected StreamExecutionEnvironment createPipeline(ParameterTool params) {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+import java.io.IOException;
+import java.util.*;
 
-        DataStream<Message> source = createSource(env, params);
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 
-        // TODO - apply any filtering and index projection logic here
+@Slf4j
+public abstract class SolrJob extends SearchIndexJob {
 
-        writeResults(source, params);
+    private static final String PARAMS_INDEX_WINDOW_MAX_MS = "index.time.ms";
+    private static final long DEFAULT_INDEX_WINDOW_MAX_MS = 10000;
+    private static final long DEFAULT_MAX_EVENTS = 10000;
 
-        return env;
+    @Override
+    protected Map<String, Set<String>> loadFieldsFromIndex(ParameterTool params) throws IOException {
+        SolrClient solrClient = SolrClientBuilder.builder()
+                .solrUrls(Arrays.asList(params.getRequired("solr.urls").split(",")))
+                .build().build();
+        try {
+            List<String> collections = CollectionAdminRequest.listCollections(solrClient);
+            return collections.stream().collect(toMap(
+                    collection -> collection,
+                    collection -> {
+                        SchemaRequest.Fields request = new SchemaRequest.Fields();
+                        try {
+                            SchemaResponse.FieldsResponse response = request.process(solrClient, collection);
+                            return response.getFields().stream().map(m -> m.get("name").toString()).collect(toSet());
+                        } catch (SolrServerException | IOException e) {
+                            log.error("Problem with Solr Schema inspection", e);
+                            return new HashSet<String>();
+                        }
+                    }));
+        } catch (SolrServerException e) {
+            throw new IOException(e);
+        }
     }
+
+    protected void writeResults(DataStream<IndexEntry> results, ParameterTool params) {
+        results.keyBy(IndexEntry::getIndex).timeWindow(Time.milliseconds(params.getLong(PARAMS_INDEX_WINDOW_MAX_MS, DEFAULT_INDEX_WINDOW_MAX_MS)))
+                .trigger(EventTimeAndCountTrigger.of(DEFAULT_MAX_EVENTS))
+                .apply(new SolrIndexer(params))
+                .name("Solr Indexer")
+                .uid("Solr Indexer");
+    }
+
     protected abstract DataStream<Message> createSource(StreamExecutionEnvironment env, ParameterTool params);
-    protected abstract void writeResults(DataStream<Message> results, ParameterTool params);
+
 }
