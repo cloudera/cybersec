46eb8e683984fa60cc329f5efc6484fd42f00deb
Simon Elliston Ball <simon@simonellistonball.com>
2020-08-27 13:57:14 +0100

    Initial solr implementation of indexing

--- flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexer/SolrJobKafka.java
+++ flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexer/SolrJobKafka.java
@@ -2,20 +2,24 @@ package com.cloudera.cyber.indexer;
 
 import com.cloudera.cyber.Message;
 import com.cloudera.cyber.flink.FlinkUtils;
+import com.cloudera.cyber.indexing.MessageRetry;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Preconditions;
 
+import java.io.IOException;
+
 import static com.cloudera.cyber.flink.ConfigConstants.PARAMS_TOPIC_INPUT;
 
 public class SolrJobKafka extends SolrJob {
+
+    private static final String PARAMS_RETRY_TOPIC = "retry.topic";
 
     public static void main(String[] args) throws Exception {
         Preconditions.checkArgument(args.length == 1, "Arguments must consist of a single properties file");
         new SolrJobKafka().createPipeline(ParameterTool.fromArgs(args)).execute("Indexing - Solr");
     }
-
 
     @Override
     public DataStream<Message> createSource(StreamExecutionEnvironment env, ParameterTool params) {
@@ -25,7 +29,14 @@ public class SolrJobKafka extends SolrJob {
     }
 
     @Override
-    protected void writeResults(DataStream<Message> results, ParameterTool params) {
-        throw new IllegalArgumentException("Solr not yet implemented");
+    protected DataStream<MessageRetry> createRetrySource(StreamExecutionEnvironment env, ParameterTool params) {
+        return env.addSource(
+                new FlinkUtils(MessageRetry.class).createKafkaGenericSource(params.getRequired(PARAMS_RETRY_TOPIC), params, "indexer-solr"));
     }
+
+    @Override
+    protected void writeRetryResults(DataStream<MessageRetry> retries, ParameterTool params) throws IOException {
+        retries.addSink(new FlinkUtils<>(MessageRetry.class).createKafkaSink(params.getRequired(PARAMS_RETRY_TOPIC), params));
+    }
+
 }
