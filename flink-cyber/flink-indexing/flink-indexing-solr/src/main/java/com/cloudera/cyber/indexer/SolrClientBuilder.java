0c3937cf822fa85a78363a558f63888d639a4b6b
Simon Elliston Ball <simon@simonellistonball.com>
2020-08-27 13:57:14 +0100

    Initial solr implementation of indexing

--- flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexer/SolrClientBuilder.java
+++ flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexer/SolrClientBuilder.java
@@ -1,4 +1,34 @@
 package com.cloudera.cyber.indexer;
 
+import lombok.Data;
+import lombok.Builder;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
+import org.apache.solr.client.solrj.impl.HttpClientUtil;
+import org.apache.solr.client.solrj.impl.Krb5HttpClientBuilder;
+import org.apache.solr.client.solrj.impl.SolrHttpClientBuilder;
+
+import java.util.List;
+
+@Data
+@Builder
 public class SolrClientBuilder {
+    private List<String> solrUrls;
+    private String trustStorePath;
+    private String trustStorePassword;
+
+    public SolrClient build() {
+        if (trustStorePath != null) {
+            System.setProperty("javax.net.ssl.trustStore", trustStorePath);
+        }
+
+        if (trustStorePassword != null) {
+            System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword);
+        }
+
+        Krb5HttpClientBuilder krbBuild = new Krb5HttpClientBuilder();
+        SolrHttpClientBuilder kb = krbBuild.getBuilder();
+        HttpClientUtil.setHttpClientBuilder(kb);
+        return new CloudSolrClient.Builder(solrUrls).build();
+    }
 }
--- flink-cyber/flink-indexing/flink-indexing-solr/src/test/java/com/cloudera/cyber/indexer/SolrJobTest.java
+++ flink-cyber/flink-indexing/flink-indexing-solr/src/test/java/com/cloudera/cyber/indexer/SolrJobTest.java
@@ -1,18 +1,25 @@
 package com.cloudera.cyber.indexer;
 
 import com.cloudera.cyber.Message;
+import com.cloudera.cyber.indexing.IndexEntry;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.test.util.JobTester;
+import org.apache.flink.test.util.ManualSource;
 
 public class SolrJobTest extends SolrJob {
+    private ManualSource<Message> source;
+
     @Override
     protected DataStream<Message> createSource(StreamExecutionEnvironment env, ParameterTool params) {
-        return null;
+        source = JobTester.createManualSource(env, TypeInformation.of(Message.class));
+        return source.getDataStream();
     }
 
     @Override
-    protected void writeResults(DataStream<Message> results, ParameterTool params) {
+    protected void writeResults(DataStream<IndexEntry> results, ParameterTool params) {
 
     }
 }
