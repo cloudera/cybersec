eb7370960a6a6e5054cf34b7ba24dd07c08e3184
Simon Elliston Ball <simon@simonellistonball.com>
2020-10-01 12:45:47 +0100

    Added Simple Hive indexer

---
+++ flink-cyber/flink-indexing/flink-indexing-hive/README.md
@@ -0,0 +1,36 @@
+# Hive Indexer
+
+This job takes Message objects from a Kafka Topic and translates them to fit into a Hive Table.
+
+The process reads the structure of the hive table and extracts the fields required to populate it from the message. It also expects a minimum set of fields in the hive table, and a column which accepts a map to contain any non-extracted fields.
+
+For example, the following hive table schema could be used as a destination:
+```SQL
+CREATE TABLE `events`(                    
+   `id` string,                                     
+   `ts` timestamp,                                  
+   `message` string,                                
+   `fields` map<string,string>,                     
+   `ip_src_addr` string,                            
+   `ip_dst_addr` string,                            
+   `ip_src_port` string,                          
+   `ip_dst_port` string)
+PARTITIONED BY (source string) 
+STORED AS ORC;
+```
+
+| parameter | default | explanation |
+| --- | --- | --- |
+| hive.table | events | Table to publish messages to |
+| hive.catalog | default | A catalog to store events in |
+| hive.schema | cyber | The name of the database in Hive |
+|||
+| topic.input || Source topic in Kafka |
+| kafka.*|| Ability to override Kafka Consumer settings |
+| schema.registry.url|http://<schema-regsitry-host>:7788/api/v1| location of schema registry|
+| schema.registry.client.ssl|| whether to use ssl for SR |
+| trustStorePath|| Location of truststore for SR use |
+| trustStorePassword|| Password for truststore |
+| keyStorePassword|| Password for keystore used to access SR |
+
+Note that the fixed fields include the UUID for the mesage, the timestamp and message field (a basic string describing the message) and fields (a generic map holding all the fields, flattened).
---
+++ flink-cyber/flink-indexing/flink-indexing-hive/src/main/java/com/cloudera/cyber/indexing/hive/FieldExtractor.java
@@ -0,0 +1,71 @@
+package com.cloudera.cyber.indexing.hive;
+
+import com.cloudera.cyber.Message;
+import com.google.common.collect.Streams;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.types.Row;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
+public class FieldExtractor extends RichMapFunction<Message, Row> {
+
+    List<String> listFields;
+    protected static final List<String> headerFields = Arrays.asList("source", "id", "ts", "message", "fields");
+
+    public FieldExtractor(List<String> listFields) {
+        this.listFields = listFields.stream().filter(n -> !FieldExtractor.headerFields.contains(n)).collect(toList());
+    }
+
+    public List<String> fieldNames() {
+        return Stream.concat(headerFields.stream(),
+                listFields.stream()).collect(toList());
+    }
+
+    @Override
+    public Row map(Message m) throws Exception {
+        return Row.of(Stream.of(
+                Stream.of(m.getId(), m.getTs(), m.getMessage(), flattenToStrings(m.getExtensions(), listFields)),
+                listFields.stream().map(field -> m.getExtensions().get(field)),
+                Stream.of(m.getSource())
+        ).flatMap(s->s).toArray());
+    }
+
+    /**
+     * TODO - properly recurse this.
+     *
+     * @param extensions
+     * @param filter
+     * @return
+     */
+    private static Map<String, String> flattenToStrings(Map<String, Object> extensions, List<String> filter) {
+        return extensions.entrySet().stream()
+                .filter(e -> !filter.contains(e.getKey()))
+                .collect(toMap(
+                        k -> k.getKey(),
+                        v -> v.getValue().toString())
+                );
+    }
+
+    public TypeInformation<Row> type() {
+        String[] names = fieldNames().toArray(new String[fieldNames().size()]);
+
+        TypeInformation<?>[] baseTypes = {
+                Types.STRING, // id
+                Types.SQL_TIMESTAMP, // ts
+                Types.STRING, // message
+                Types.MAP(Types.STRING, Types.STRING) //fields
+        };
+        Stream<TypeInformation<?>> typeInformationStream = listFields.stream().map(n -> Types.STRING);
+
+        List<TypeInformation<?>> allTypes = Stream.of(Stream.of(baseTypes), typeInformationStream, Stream.of(Types.STRING)).flatMap(s->s).collect(toList());
+        return Types.ROW_NAMED(names, allTypes.toArray(new TypeInformation<?>[allTypes.size()]));
+    }
+}
