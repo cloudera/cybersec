12d0d9fbf13f6b73af72ff336a892c21c3b0f8a9
Simon Elliston Ball <simon@simonellistonball.com>
2020-11-13 16:30:27 +0000

    Refactor ES indexer based on Solr and Search Common work

--- flink-cyber/flink-indexing/flink-indexing-elastic/src/main/java/com/cloudera/cyber/indexing/elastic/ElasticJobKafka.java
+++ flink-cyber/flink-indexing/flink-indexing-elastic/src/main/java/com/cloudera/cyber/indexing/elastic/ElasticJobKafka.java
@@ -2,18 +2,29 @@ package com.cloudera.cyber.indexing.elastic;
 
 import com.cloudera.cyber.Message;
 import com.cloudera.cyber.flink.FlinkUtils;
-import com.cloudera.cyber.indexing.MessageRetry;
+import com.cloudera.cyber.indexing.CollectionField;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
 import org.apache.flink.util.Preconditions;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.util.Properties;
 
 import static com.cloudera.cyber.flink.ConfigConstants.PARAMS_TOPIC_INPUT;
+import static com.cloudera.cyber.flink.Utils.readKafkaProperties;
 
+@Slf4j
 public class ElasticJobKafka extends ElasticJob {
-    private static final String PARAMS_RETRY_TOPIC = "retry.topic";
+
+    private static final String DEFAULT_TOPIC_CONFIG_LOG = "es.config.log";
 
     public static void main(String[] args) throws Exception {
         Preconditions.checkArgument(args.length == 1, "Arguments must consist of a single properties file");
@@ -29,14 +40,36 @@ public class ElasticJobKafka extends ElasticJob {
     }
 
     @Override
-    protected DataStream<MessageRetry> createRetrySource(StreamExecutionEnvironment env, ParameterTool params) {
-        return env.addSource(
-                new FlinkUtils(MessageRetry.class).createKafkaGenericSource(params.getRequired(PARAMS_RETRY_TOPIC), params, "indexer-solr"));
+    protected DataStream<CollectionField> createConfigSource(StreamExecutionEnvironment env, ParameterTool params) {
+        DataStreamSource<CollectionField> dataStreamSource = env.addSource(
+                new ElasticTemplateFieldsSource(client,
+                        params.getLong(PARAMS_SCHEMA_REFRESH_INTERVAL, DEFAULT_SCHEMA_REFRESH_INTERVAL))
+        );
+        return dataStreamSource
+                .name("Schema Stream").uid("schema-stream")
+                .setMaxParallelism(1)
+                .setParallelism(1);
     }
 
     @Override
-    protected void writeRetryResults(DataStream<MessageRetry> retries, ParameterTool params) throws IOException {
-        retries.addSink(new FlinkUtils<>(MessageRetry.class).createKafkaSink(params.getRequired(PARAMS_RETRY_TOPIC), params));
+    protected void logConfig(DataStream<CollectionField> configSource, ParameterTool params) {
+        String topic = params.get(PARAMS_TOPIC_CONFIG_LOG, DEFAULT_TOPIC_CONFIG_LOG);
+        Properties kafkaProperties = readKafkaProperties(params, false);
+        log.info("Creating Kafka Sink for {}, using {}", topic, kafkaProperties);
+        FlinkKafkaProducer<CollectionField> kafkaSink = new FlinkKafkaProducer<>(topic,
+                (KafkaSerializationSchema<CollectionField>) (collectionField, aLong) -> {
+                    ObjectMapper om = new ObjectMapper();
+                    try {
+                        return new ProducerRecord<>(topic, null, Instant.now().toEpochMilli(), collectionField.getKey().getBytes(), om.writeValueAsBytes(collectionField));
+                    } catch (IOException e) {
+                        return new ProducerRecord<>(topic, null, Instant.now().toEpochMilli(), collectionField.getKey().getBytes(), e.getMessage().getBytes());
+                    }
+                },
+                kafkaProperties,
+                FlinkKafkaProducer.Semantic.AT_LEAST_ONCE);
+
+        configSource.addSink(kafkaSink).name("Schema Log").uid("schema-log")
+                .setParallelism(1);
     }
 
 }
