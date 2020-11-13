3ea72999ec33e9ea14318123758980b2f23a064c
Simon Elliston Ball <simon@simonellistonball.com>
2020-11-13 16:31:43 +0000

    Solr indexer corrections

--- flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexing/SolrJob.java
+++ flink-cyber/flink-indexing/flink-indexing-solr/src/main/java/com/cloudera/cyber/indexing/SolrJob.java
@@ -7,17 +7,6 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.request.CollectionAdminRequest;
-import org.apache.solr.client.solrj.request.schema.SchemaRequest;
-import org.apache.solr.client.solrj.response.schema.SchemaResponse;
-
-import java.io.IOException;
-import java.util.*;
-
-import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.toSet;
 
 @Slf4j
 public abstract class SolrJob extends SearchIndexJob {
@@ -25,30 +14,6 @@ public abstract class SolrJob extends SearchIndexJob {
     private static final String PARAMS_INDEX_WINDOW_MAX_MS = "index.time.ms";
     private static final long DEFAULT_INDEX_WINDOW_MAX_MS = 10000;
     private static final long DEFAULT_MAX_EVENTS = 10000;
-
-    @Override
-    protected Map<String, Set<String>> loadFieldsFromIndex(ParameterTool params) throws IOException {
-        SolrClient solrClient = SolrClientBuilder.builder()
-                .solrUrls(Arrays.asList(params.getRequired("solr.urls").split(",")))
-                .build().build();
-        try {
-            List<String> collections = CollectionAdminRequest.listCollections(solrClient);
-            return collections.stream().collect(toMap(
-                    collection -> collection,
-                    collection -> {
-                        SchemaRequest.Fields request = new SchemaRequest.Fields();
-                        try {
-                            SchemaResponse.FieldsResponse response = request.process(solrClient, collection);
-                            return response.getFields().stream().map(m -> m.get("name").toString()).collect(toSet());
-                        } catch (SolrServerException | IOException e) {
-                            log.error("Problem with Solr Schema inspection", e);
-                            return new HashSet<String>();
-                        }
-                    }));
-        } catch (SolrServerException e) {
-            throw new IOException(e);
-        }
-    }
 
     protected void writeResults(DataStream<IndexEntry> results, ParameterTool params) {
         results.keyBy(IndexEntry::getIndex).timeWindow(Time.milliseconds(params.getLong(PARAMS_INDEX_WINDOW_MAX_MS, DEFAULT_INDEX_WINDOW_MAX_MS)))
@@ -59,5 +24,4 @@ public abstract class SolrJob extends SearchIndexJob {
     }
 
     protected abstract DataStream<Message> createSource(StreamExecutionEnvironment env, ParameterTool params);
-
 }
