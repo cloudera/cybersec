fecff32351e050f840df6ec024ba111eab68cb7d
Simon Elliston Ball <simon@simonellistonball.com>
2020-11-18 03:14:13 +0000

    Minor tidying

--- flink-cyber/flink-rules/src/main/java/com/cloudera/cyber/rules/engines/JavascriptEngineBuilder.java
+++ flink-cyber/flink-rules/src/main/java/com/cloudera/cyber/rules/engines/JavascriptEngineBuilder.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = true)
 public class JavascriptEngineBuilder extends RuleEngineBuilder<JavaScriptEngine> {
     @Override
     public JavaScriptEngine build() {
