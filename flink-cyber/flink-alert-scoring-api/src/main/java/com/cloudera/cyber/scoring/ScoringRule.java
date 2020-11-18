098f7b6e82f0de9b0bd30f84c308f5c86426ad7d
Simon Elliston Ball <simon@simonellistonball.com>
2020-11-18 03:12:45 +0000

    Started to refactor the rules and scoring engines into avro friendly form. Needs redesign.

--- flink-cyber/flink-alert-scoring-api/src/main/java/com/cloudera/cyber/scoring/ScoringRule.java
+++ flink-cyber/flink-alert-scoring-api/src/main/java/com/cloudera/cyber/scoring/ScoringRule.java
@@ -1,25 +1,32 @@
 package com.cloudera.cyber.scoring;
 
+import com.cloudera.cyber.AvroTypes;
 import com.cloudera.cyber.Message;
 import com.cloudera.cyber.rules.BaseDynamicRule;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import com.cloudera.cyber.rules.RuleType;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.apache.avro.AvroRuntimeException;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.specific.SpecificRecord;
 
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 
+import static java.util.stream.Collectors.toList;
+
 @Data
 @SuperBuilder(toBuilder = true)
-@AllArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class ScoringRule extends BaseDynamicRule {
-    UUID id;
-    boolean enabled = true;
-    int order;
+public class ScoringRule extends BaseDynamicRule implements SpecificRecord {
+    @Builder.Default
+    private UUID id = UUID.randomUUID();
+    private boolean enabled = true;
+    private int order;
 
     public static final String RESULT_SCORE = "score";
     public static final String RESULT_REASON = "reason";
@@ -38,5 +45,56 @@ public class ScoringRule extends BaseDynamicRule {
         return getType().engine(getRuleScript()).feed(message);
     }
 
+    public static final Schema SCHEMA$ = SchemaBuilder
+            .record(ScoringRule.class.getName())
+            .namespace(ScoringRule.class.getPackage().getName())
+            .fields()
+            .requiredString("name")
+            .requiredInt("order")
+            .requiredLong("tsStart")
+            .requiredLong("tsEnd")
+            .name("type").type(Schema.createEnum(
+                    RuleType.class.getName(),
+                    "",
+                    RuleType.class.getPackage().getName(),
+                    Arrays.stream(RuleType.values()).map(s->s.name()).collect(toList()))).noDefault()
+            .requiredString("ruleScript")
+            .name("id").type(AvroTypes.uuidType).noDefault()
+            .requiredBoolean("enabled")
+            .endRecord();
 
+    @Override
+    public Schema getSchema() {
+        return SCHEMA$;
+    }
+
+    @Override
+    public Object get(int field$) {
+        switch (field$) {
+            case 0: return getName();
+            case 1: return order;
+            case 2: return getTsStart();
+            case 3: return getTsEnd();
+            case 4: return getType();
+            case 5: return getRuleScript();
+            case 6: return id.toString();
+            case 7: return enabled;
+            default: throw new AvroRuntimeException("Bad index");
+        }
+    }
+
+    @Override
+    public void put(int field$, Object value$) {
+        switch (field$) {
+            case 0: this.setName(value$.toString()); break;
+            case 1: this.setOrder((int) value$); break;
+            case 2: this.setTsStart(Instant.ofEpochMilli((long)value$)); break;
+            case 3: this.setTsEnd(Instant.ofEpochMilli((long)value$)); break;
+            case 4: this.setType(RuleType.valueOf(value$.toString())); break;
+            case 5: this.setRuleScript(value$.toString()); break;
+            case 6: this.setId(UUID.fromString(value$.toString())); break;
+            case 7: this.setEnabled((boolean) value$); break;
+            default: throw new AvroRuntimeException("Bad index");
+        }
+    }
 }
---
+++ flink-cyber/flink-alert-scoring-api/src/test/java/SerializationTests.java
@@ -0,0 +1,57 @@
+import com.cloudera.cyber.rules.RuleType;
+import com.cloudera.cyber.scoring.Scores;
+import com.cloudera.cyber.scoring.ScoringRule;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.formats.avro.typeutils.AvroTypeInfo;
+import org.apache.flink.util.InstantiationUtil;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.time.Instant;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class SerializationTests {
+
+    @Test
+    public void testScore() throws IOException {
+        Scores test = Scores.builder().reason("test").score(1.0).build();
+        Scores out = test(test);
+        assertThat(out, equalTo(test));
+    }
+
+    @Test
+    @Ignore
+    public void testScoringRule() throws IOException {
+        ScoringRule test = ScoringRule.builder()
+                .enabled(true)
+                .name("test")
+                .order(1)
+                .ruleScript("test()")
+                .tsStart(Instant.MIN)
+                .tsEnd(Instant.MAX)
+                .type(RuleType.JS)
+                .build();
+
+        fail("Cannot serialize because of base class");
+    }
+
+
+    public static <T extends SpecificRecordBase> T test(T obj) throws IOException {
+        Class cls = obj.getClass();
+        AvroTypeInfo<T> ti = new AvroTypeInfo<T>(cls);
+        TypeSerializer<T> serializer = ti.createSerializer(new ExecutionConfig());
+
+        byte[] bytes = InstantiationUtil.serializeToByteArray(serializer, obj);
+        T out = InstantiationUtil.deserializeFromByteArray(serializer, bytes);
+
+        assertThat(out, notNullValue());
+        return out;
+    }
+}
