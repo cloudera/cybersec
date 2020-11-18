43166b452591238ac5881a6919b7164540a3b61a
Simon Elliston Ball <simon@simonellistonball.com>
2020-11-18 03:11:43 +0000

    Refactored Scoring Test

--- flink-cyber/flink-alert-scoring/src/test/java/com/cloudera/cyber/scoring/TestScoringJob.java
+++ flink-cyber/flink-alert-scoring/src/test/java/com/cloudera/cyber/scoring/TestScoringJob.java
@@ -43,13 +43,11 @@ public class TestScoringJob extends ScoringJob {
     public void testPipeline() throws Exception {
         StreamExecutionEnvironment env = createPipeline(ParameterTool.fromMap(Collections.emptyMap()));
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-        env.enableCheckpointing(100l);
+        env.setParallelism(1);
+
         JobTester.startTest(env);
 
         UUID ruleId = UUID.randomUUID();
-
-        source.sendWatermark(0l);
-        querySource.sendWatermark(0l);
 
         sendRule(ScoringRuleCommand.builder()
                 .type(UPSERT)
@@ -73,20 +71,14 @@ public class TestScoringJob extends ScoringJob {
                 .ts(100l)
                 .extensions(Collections.singletonMap("test", "test-value"))
                 .originalSource(TestUtils.source("test", 0, 0))
+                .source("test")
                 .build());
 
         source.sendWatermark(100l);
         querySource.sendWatermark(100l);
 
-        ScoredMessage message = sink.poll();
-        assertThat("message got scored", message.getScores(), hasSize(1));
-
         sendRule(ScoringRuleCommand.builder()
                 .type(LIST).ts(900).id(UUID.randomUUID()).build());
-
-        DynamicRuleCommandResult<ScoringRule> poll1 = queryResponse.poll(Duration.ofMillis(1000));
-        assertThat(poll1.getRule(), hasProperty("name", equalTo("test-rule")));
-
 
         sendRule(ScoringRuleCommand.builder()
                 .type(DELETE)
@@ -94,20 +86,28 @@ public class TestScoringJob extends ScoringJob {
                 .id(UUID.randomUUID())
                 .ruleId(ruleId).build());
 
-        DynamicRuleCommandResult<ScoringRule> poll2 = queryResponse.poll(Duration.ofMillis(1000));
-
-        Thread.sleep(100);
-
         sendRecord(Message.builder()
                 .extensions(Collections.singletonMap("test", "test-value2"))
                 .ts(2000l)
                 .originalSource(TestUtils.source("test", 0, 0))
+                .source("test")
                 .build());
 
-        ScoredMessage message1 = sink.poll(Duration.ofMillis(5000));
-        assertThat("message got scored", message1.getScores(), nullValue());
+        source.sendWatermark(3000l);
+        querySource.sendWatermark(3000l);
 
         JobTester.stopTest();
+
+        ScoredMessage message = sink.poll();
+        assertThat("message got scored", message.getScores(), hasSize(1));
+
+        DynamicRuleCommandResult<ScoringRule> poll1 = queryResponse.poll(Duration.ofMillis(1000));
+        assertThat(poll1.getRule(), hasProperty("name", equalTo("test-rule")));
+
+        DynamicRuleCommandResult<ScoringRule> poll2 = queryResponse.poll(Duration.ofMillis(1000));
+
+        ScoredMessage message1 = sink.poll();
+        assertThat("message got scored", message1.getScores(), nullValue());
 
     }
 
