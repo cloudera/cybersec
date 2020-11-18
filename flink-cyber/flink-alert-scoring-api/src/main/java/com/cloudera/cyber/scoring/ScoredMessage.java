e592353bff87a64706a7b549a729a4f9a2a60cbb
Simon Elliston Ball <simon@simonellistonball.com>
2020-11-18 03:12:45 +0000

    Started to refactor the rules and scoring engines into avro friendly form. Needs redesign.

--- flink-cyber/flink-alert-scoring-api/src/main/java/com/cloudera/cyber/scoring/ScoredMessage.java
+++ flink-cyber/flink-alert-scoring-api/src/main/java/com/cloudera/cyber/scoring/ScoredMessage.java
@@ -2,18 +2,28 @@ package com.cloudera.cyber.scoring;
 
 import com.cloudera.cyber.IdentifiedMessage;
 import com.cloudera.cyber.Message;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import com.cloudera.cyber.rules.RuleType;
+import lombok.*;
+import org.apache.avro.AvroRuntimeException;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.avro.specific.SpecificRecordBase;
 
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
+
+import static java.util.stream.Collectors.toList;
 
 @Getter
 @EqualsAndHashCode
 @ToString
 @Builder(toBuilder = true)
-public class ScoredMessage implements IdentifiedMessage {
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScoredMessage extends SpecificRecordBase implements IdentifiedMessage, SpecificRecord {
     private Message message;
     private List<Scores> scores;
     private List<ScoringRule> rules;
@@ -26,5 +36,49 @@ public class ScoredMessage implements IdentifiedMessage {
     @Override
     public long getTs() {
         return message.getTs();
+    }
+
+    public static final Schema SCHEMA$ = SchemaBuilder.record(ScoredMessage.class.getName())
+            .namespace(ScoredMessage.class.getPackage().getName())
+            .fields()
+            .name("message").type(Message.SCHEMA$).noDefault()
+            .name("scores").type(Schema.createArray(Scores.SCHEMA$)).noDefault()
+            .name("rules").type(Schema.createArray(ScoringRule.SCHEMA$)).noDefault()
+            .endRecord();
+
+    @Override
+    public Schema getSchema() {
+        return SCHEMA$;
+    }
+
+    @Override
+    public Object get(int field$) {
+        switch (field$) {
+            case 0:
+                return message;
+            case 1:
+                return scores;
+            case 2:
+                return rules;
+            default:
+                throw new AvroRuntimeException("Bad index");
+        }
+    }
+
+    @Override
+    public void put(int field$, Object value$) {
+        switch (field$) {
+            case 0:
+                this.message = (Message) value$;
+                break;
+            case 1:
+                this.scores = (List<Scores>) value$;
+                break;
+            case 2:
+                this.rules = (List<ScoringRule>) value$;
+                break;
+            default:
+                throw new AvroRuntimeException("Bad index");
+        }
     }
 }
--- flink-cyber/flink-alert-scoring-api/src/main/java/com/cloudera/cyber/scoring/Scores.java
+++ flink-cyber/flink-alert-scoring-api/src/main/java/com/cloudera/cyber/scoring/Scores.java
@@ -1,15 +1,58 @@
 package com.cloudera.cyber.scoring;
 
+import com.cloudera.cyber.AvroTypes;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.avro.AvroRuntimeException;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.avro.specific.SpecificRecordBase;
 
 import java.util.UUID;
 
 @Data
 @Builder
-public class Scores {
-    private UUID ruleId;
+@NoArgsConstructor
+@AllArgsConstructor
+public class Scores extends SpecificRecordBase implements SpecificRecord {
+    @Builder.Default
+    private UUID ruleId = UUID.randomUUID();
     private Double score;
     private String reason;
 
+    public static final Schema SCHEMA$ = SchemaBuilder.record(Scores.class.getName())
+            .namespace(Scores.class.getPackage().getName())
+            .fields()
+            .name("ruleId").type(AvroTypes.uuidType).noDefault()
+            .requiredDouble("score")
+            .requiredString("reason")
+            .endRecord();
+
+    @Override
+    public Schema getSchema() {
+        return SCHEMA$;
+    }
+
+    @Override
+    public Object get(int field$) {
+        switch (field$) {
+            case 0: return ruleId;
+            case 1: return score;
+            case 2: return reason;
+            default: throw new AvroRuntimeException("Bad index");
+        }
+    }
+
+    @Override
+    public void put(int field$, Object value$) {
+        switch (field$) {
+            case 0: ruleId = (UUID) value$; break;
+            case 1: score = (Double)value$; break;
+            case 2: reason = value$.toString(); break;
+            default: throw new AvroRuntimeException("Bad Index");
+        }
+    }
 }
