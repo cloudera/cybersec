6f5a02601b7a05cbbc485565446a3158a9d49759
Simon Elliston Ball <simon@simonellistonball.com>
2020-11-24 03:30:08 +0000

    Dynamic rules changes to Avro Schema

--- flink-cyber/flink-rules/src/main/java/com/cloudera/cyber/rules/BaseDynamicRule.java
+++ flink-cyber/flink-rules/src/main/java/com/cloudera/cyber/rules/BaseDynamicRule.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
+import org.apache.avro.specific.SpecificRecordBase;
 
 import java.time.Instant;
 import java.util.Map;
@@ -13,10 +13,9 @@ import java.util.Map;
 
 @Getter
 @Setter
-@SuperBuilder(toBuilder = true)
 @NoArgsConstructor
 @ToString
-public abstract class BaseDynamicRule implements DynamicRule {
+public abstract class BaseDynamicRule extends SpecificRecordBase implements DynamicRule {
     private String name;
     private int order;
 
@@ -27,8 +26,64 @@ public abstract class BaseDynamicRule implements DynamicRule {
 
     private String ruleScript;
 
+    protected BaseDynamicRule(BaseDynamicRuleBuilder<?, ?> b) {
+        this.name = b.name;
+        this.order = b.order;
+        this.tsStart = b.tsStart;
+        this.tsEnd = b.tsEnd;
+        this.type = b.type;
+        this.ruleScript = b.ruleScript;
+    }
+
     @Override
     public Map<String, Object> apply(Message message) {
         return getType().engine(ruleScript).feed(message);
+    }
+
+    public static abstract class BaseDynamicRuleBuilder<C extends BaseDynamicRule, B extends BaseDynamicRuleBuilder<C, B>> {
+        private String name;
+        private int order;
+        private Instant tsStart;
+        private Instant tsEnd;
+        private RuleType type;
+        private String ruleScript;
+
+        public B name(String name) {
+            this.name = name;
+            return self();
+        }
+
+        public B order(int order) {
+            this.order = order;
+            return self();
+        }
+
+        public B tsStart(Instant tsStart) {
+            this.tsStart = tsStart;
+            return self();
+        }
+
+        public B tsEnd(Instant tsEnd) {
+            this.tsEnd = tsEnd;
+            return self();
+        }
+
+        public B type(RuleType type) {
+            this.type = type;
+            return self();
+        }
+
+        public B ruleScript(String ruleScript) {
+            this.ruleScript = ruleScript;
+            return self();
+        }
+
+        protected abstract B self();
+
+        public abstract C build();
+
+        public String toString() {
+            return "BaseDynamicRule.BaseDynamicRuleBuilder(super=" + super.toString() + ", name=" + this.name + ", order=" + this.order + ", tsStart=" + this.tsStart + ", tsEnd=" + this.tsEnd + ", type=" + this.type + ", ruleScript=" + this.ruleScript + ")";
+        }
     }
 }
--- flink-cyber/flink-rules/src/main/java/com/cloudera/cyber/rules/DynamicRuleCommand.java
+++ flink-cyber/flink-rules/src/main/java/com/cloudera/cyber/rules/DynamicRuleCommand.java
@@ -3,20 +3,54 @@ package com.cloudera.cyber.rules;
 import com.cloudera.cyber.Timestamped;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.apache.avro.AvroRuntimeException;
+import org.apache.avro.specific.SpecificRecordBase;
 
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@SuperBuilder
 @ToString
-public class DynamicRuleCommand<T extends DynamicRule> implements Timestamped {
+@SuperBuilder
+public abstract class DynamicRuleCommand<T extends DynamicRule> extends SpecificRecordBase implements Timestamped {
     @NonNull protected UUID id;
     @NonNull protected DynamicRuleCommandType type;
     @NonNull protected long ts;
     protected UUID ruleId = null;
     protected T rule = null;
 
-    public long getTs() { return ts; }
+    protected DynamicRuleCommand(DynamicRuleCommandBuilder<T, ?, ?> b) {
+        this.id = b.id;
+        this.type = b.type;
+        this.ts = b.ts;
+        this.ruleId = b.ruleId;
+        this.rule = b.rule;
+    }
+
+    @Override
+    public Object get(int field$) {
+        switch (field$) {
+            case 0: return id;
+            case 1: return type;
+            case 2: return ts;
+            case 3: return ruleId;
+            case 4: return rule;
+            default: throw new AvroRuntimeException("Bad index");
+        }
+    }
+
+    @Override
+    public void put(int field$, Object value$) {
+        switch (field$) {
+            case 0: id = UUID.fromString(value$.toString()); break;
+            case 1: type = DynamicRuleCommandType.valueOf(value$.toString()); break;
+            case 2: ts = (long) value$; break;
+            case 3: ruleId = UUID.fromString(value$.toString()); break;
+            case 4: rule = (T)value$; break;
+
+            default: throw new AvroRuntimeException("Bad index");
+        }
+    }
+
 }
