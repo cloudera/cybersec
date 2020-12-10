ce5681b12a2fb70d4df084090e4fb64706c4d972
cduby <carolynduby@ieee.org>
2020-12-10 12:59:31 -0500

    Hive streaming indexer using custom sink with checkpoints

---
+++ flink-cyber/flink-indexing/flink-indexing-hive/src/main/java/com/cloudera/cyber/indexing/hive/HiveJobKafka.java
@@ -0,0 +1,30 @@
+package com.cloudera.cyber.indexing.hive;
+
+import com.cloudera.cyber.Message;
+import com.cloudera.cyber.flink.FlinkUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Preconditions;
+
+import static com.cloudera.cyber.flink.ConfigConstants.PARAMS_TOPIC_INPUT;
+
+@Slf4j
+public class HiveJobKafka extends HiveJob {
+
+    private static final String PARAMS_GROUP_ID = "kafka.group.id";
+    private static final String DEFAULT_GROUP_ID = "indexer-hive";
+
+    public static void main(String[] args) throws Exception {
+        Preconditions.checkArgument(args.length == 1, "Arguments must consist of a single properties file");
+        new HiveJobKafka().createPipeline(ParameterTool.fromPropertiesFile(args[0])).execute("Indexing - Hive");
+    }
+
+    @Override
+    protected DataStream<Message> createSource(StreamExecutionEnvironment env, ParameterTool params) {
+        return env.addSource(
+                new FlinkUtils(Message.class).createKafkaSource(params.getRequired(PARAMS_TOPIC_INPUT), params, params.get(PARAMS_GROUP_ID, DEFAULT_GROUP_ID))
+        ).name("Kafka Source").uid("kafka-source");
+    }
+}
