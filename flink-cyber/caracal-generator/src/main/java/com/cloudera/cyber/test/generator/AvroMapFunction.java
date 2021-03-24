860c4ebe5ea27524c70897ef02aad9cd88082487
vpavlenko <vpavlenko@cybervisiontech.com>
2021-03-24 16:24:43 +0200

    CYB-1 Parse customer provided avro messages in addition to text messages
    * Fixed some files due to PR comments.
    * Added unit tests.
    * Added AvroMapFunction for source produce.

---
+++ flink-cyber/caracal-generator/src/main/java/com/cloudera/cyber/test/generator/AvroMapFunction.java
@@ -0,0 +1,33 @@
+package com.cloudera.cyber.test.generator;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import org.apache.avro.Schema;
+import org.apache.avro.Schema.Parser;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+
+public class AvroMapFunction extends RichMapFunction<Tuple2<String, String>, Tuple2<String, String>> {
+
+    private final String schemaString;
+    private transient Schema schema;
+
+    AvroMapFunction(String schemaString) {
+        this.schemaString = schemaString;
+    }
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        this.schema =  new Parser().parse(schemaString);
+
+    }
+
+    @Override
+    public Tuple2<String, String> map(Tuple2<String, String> tuple2) {
+        return new Tuple2<>(tuple2.f0, new String(Utils.jsonDecodeToAvroByteArray(tuple2.f1, schema),
+                StandardCharsets.UTF_8));
+    }
+}
---
+++ flink-cyber/metron-parser-chain/parser-chains-parsers/src/test/java/com/cloudera/parserchains/parsers/AvroParserTest.java
@@ -0,0 +1,116 @@
+package com.cloudera.parserchains.parsers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.entry;
+
+import com.cloudera.parserchains.core.FieldName;
+import com.cloudera.parserchains.core.FieldValue;
+import com.cloudera.parserchains.core.Message;
+import com.google.common.collect.ImmutableMap;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.apache.avro.Schema;
+import org.apache.avro.Schema.Parser;
+import org.apache.avro.SchemaParseException;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+
+class AvroParserTest {
+
+    private final AvroParser parser = new AvroParser();
+    private static final String SCHEMA_PATH = "/avro/avro.schema";
+    private static final String BROKEN_SCHEMA = "/avro/bad-avro.schema";
+    private static final String BAD_AVRO_DATA = "/avro/bad-avro-data.avro";
+    private static final String DATA_PATH = "/avro/avro-data.avro";
+    private static final String INPUT_FIELD = "source";
+
+    @Test
+    public void testSchemaFileRead() throws IOException {
+        String schemaPath = getFileFromResource(SCHEMA_PATH).getAbsolutePath();
+
+        parser.schemaPath(schemaPath).inputField(INPUT_FIELD);
+        Message parsedMessage = parser.parse(buildMessageFromFile(DATA_PATH));
+
+        assertThat(parsedMessage.getFields()).contains(
+                entry(FieldName.of("name"), FieldValue.of("Ben")),
+                entry(FieldName.of("number"), FieldValue.of("7")),
+                entry(FieldName.of("innerRecord"), FieldValue.of("{\"age\": 13}")),
+                entry(FieldName.of("tes3"), FieldValue.of("{td3=1}")));
+    }
+
+    @Test
+    public void testSchemaDataRead() throws IOException {
+        String schemaPath = getFileFromResource(SCHEMA_PATH).getAbsolutePath();
+
+        parser.schemaPath(schemaPath).inputField(INPUT_FIELD);
+        Message parsedMessage = parser.parse(buildMessage());
+
+        assertThat(parsedMessage.getFields()).contains(
+                entry(FieldName.of("name"), FieldValue.of("Tom")),
+                entry(FieldName.of("number"), FieldValue.of("22")),
+                entry(FieldName.of("innerRecord"), FieldValue.of("{\"age\": 42}")),
+                entry(FieldName.of("tes3"), FieldValue.of("{key11=11, key22=22}")));
+    }
+
+    @Test
+    public void testIncorrectSchema() {
+        String schemaPath = getFileFromResource(BROKEN_SCHEMA).getAbsolutePath();
+
+        assertThatThrownBy(() -> parser.schemaPath(schemaPath)).isInstanceOf(SchemaParseException.class);
+    }
+
+    @Test
+    public void testNotExistSchemaRead() {
+        String schemaPath = "/some/file.schema";
+
+        assertThatThrownBy(() -> parser.schemaPath(schemaPath)).isInstanceOf(IOException.class);
+    }
+
+    @Test
+    public void testBadAvroData() throws IOException {
+        String schemaPath = getFileFromResource(SCHEMA_PATH).getAbsolutePath();
+        parser.schemaPath(schemaPath).inputField(INPUT_FIELD);
+        Message message = parser.parse(buildMessageFromFile(BAD_AVRO_DATA));
+        assertThat(message.getError()).isNotEmpty().get().isInstanceOf(IOException.class);
+    }
+
+    private static Message buildMessageFromFile(String path) throws IOException {
+        File file = getFileFromResource(path).getAbsoluteFile();
+        String dataFile = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+        return Message.builder().addField(INPUT_FIELD, dataFile).build();
+    }
+
+    private static File getFileFromResource(String path) {
+        return new File(AvroParserTest.class.getResource(path).getFile());
+    }
+
+
+    private static Message buildMessage() throws IOException {
+        Schema schema = new Parser().parse(getFileFromResource(SCHEMA_PATH));
+        Schema innerSchema = schema.getField("innerRecord").schema();
+        GenericRecordBuilder innerRecordBuilder = new GenericRecordBuilder(innerSchema);
+        innerRecordBuilder.set("age", 42);
+        GenericData.Record innerRecord = innerRecordBuilder.build();
+        GenericRecordBuilder recordBuilder = new GenericRecordBuilder(schema);
+        recordBuilder.set("name", "Tom");
+        recordBuilder.set("number", 22);
+        recordBuilder.set("innerRecord", innerRecord);
+        recordBuilder.set("tes3", ImmutableMap.of("key11", 11, "key22", 22));
+        GenericData.Record record = recordBuilder.build();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        BinaryEncoder binaryEncoder = EncoderFactory.get().directBinaryEncoder(out, null);
+        GenericDatumWriter<GenericRecord> writer = new GenericDatumWriter<>(schema);
+        writer.write(record, binaryEncoder);
+        return Message.builder().addField(INPUT_FIELD, new String(out.toByteArray(), StandardCharsets.UTF_8)).build();
+    }
+
+}
