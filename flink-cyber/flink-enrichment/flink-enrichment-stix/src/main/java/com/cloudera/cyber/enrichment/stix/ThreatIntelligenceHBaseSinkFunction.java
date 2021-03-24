7f24229a537c8bb420f5b5ba47df7af10287a91d
cduby <carolynduby@ieee.org>
2021-03-24 16:39:30 -0400

    CYB-25: Upgrade flink to CSA 1.3.0.0 and avro to 1.10.0

--- flink-cyber/flink-enrichment/flink-enrichment-stix/src/main/java/com/cloudera/cyber/enrichment/stix/ThreatIntelligenceHBaseSinkFunction.java
+++ flink-cyber/flink-enrichment/flink-enrichment-stix/src/main/java/com/cloudera/cyber/enrichment/stix/ThreatIntelligenceHBaseSinkFunction.java
@@ -2,34 +2,42 @@ package com.cloudera.cyber.enrichment.stix;
 
 import com.cloudera.cyber.ThreatIntelligence;
 import com.cloudera.cyber.flink.UUIDUtils;
-import org.apache.flink.addons.hbase.HBaseSinkFunction;
-import org.apache.hadoop.hbase.client.BufferedMutator;
+import com.cloudera.cyber.hbase.AbstractHbaseSinkFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.connector.hbase.sink.HBaseMutationConverter;
+import org.apache.hadoop.hbase.client.Mutation;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
-public class ThreatIntelligenceHBaseSinkFunction extends HBaseSinkFunction<ThreatIntelligence> {
+public class ThreatIntelligenceHBaseSinkFunction extends AbstractHbaseSinkFunction<ThreatIntelligence> {
 
     private static final byte[] cf = Bytes.toBytes("t");
     private static final byte[] indicator = Bytes.toBytes("i");
     private static final byte[] indicatorType = Bytes.toBytes("it");
 
-    public ThreatIntelligenceHBaseSinkFunction(String hTableName) {
-        super(hTableName);
-    }
+    private static final HBaseMutationConverter<ThreatIntelligence> THREAT_INTEL_MUTATION_CONVERTER = new HBaseMutationConverter<ThreatIntelligence>() {
 
-    @Override
-    public void executeMutations(ThreatIntelligence threatIntelligence, Context context, BufferedMutator bufferedMutator) throws Exception {
-        Put put = new Put(UUIDUtils.asBytes(UUID.fromString(threatIntelligence.getId())));
+        @Override
+        public void open() {
 
-        put.addColumn(cf, indicator, threatIntelligence.getObservable().getBytes(StandardCharsets.UTF_8));
-        put.addColumn(cf, indicatorType, threatIntelligence.getObservableType().getBytes(StandardCharsets.UTF_8));
+        }
 
-        // add all the other columns
+        @Override
+        public Mutation convertToMutation(ThreatIntelligence threatIntelligence) {
+            Put put = new Put(UUIDUtils.asBytes(UUID.fromString(threatIntelligence.getId())));
 
-        bufferedMutator.mutate(put);
+            put.addColumn(cf, indicator, threatIntelligence.getObservable().getBytes(StandardCharsets.UTF_8));
+            put.addColumn(cf, indicatorType, threatIntelligence.getObservableType().getBytes(StandardCharsets.UTF_8));
+            // add other columns
+            return put;
+        }
+    };
+
+    public ThreatIntelligenceHBaseSinkFunction(String hTableName, ParameterTool params) {
+        super(hTableName, THREAT_INTEL_MUTATION_CONVERTER, params, "numThreatIntel");
     }
 
 }
