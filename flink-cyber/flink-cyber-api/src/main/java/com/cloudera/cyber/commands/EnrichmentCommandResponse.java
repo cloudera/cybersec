1e9615985ca0d814c83e69fd607c05a256ff6036
cduby <carolynduby@ieee.org>
2022-09-05 22:47:01 -0400

    [CYB-92]: fix LOCAL enrichments

--- flink-cyber/flink-cyber-api/src/main/java/com/cloudera/cyber/commands/EnrichmentCommandResponse.java
+++ flink-cyber/flink-cyber-api/src/main/java/com/cloudera/cyber/commands/EnrichmentCommandResponse.java
@@ -2,15 +2,18 @@ package com.cloudera.cyber.commands;
 
 import com.cloudera.cyber.EnrichmentEntry;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.apache.avro.Schema;
 import org.apache.avro.SchemaBuilder;
 
+import java.util.Collections;
 import java.util.List;
 
 import static com.cloudera.cyber.AvroTypes.toListOf;
 
+@EqualsAndHashCode(callSuper = true)
 @SuperBuilder
 @Data
 @NoArgsConstructor
@@ -21,8 +24,9 @@ public class EnrichmentCommandResponse extends CommandResponse<EnrichmentEntry> 
             .namespace(EnrichmentCommandResponse.class.getPackage().getName())
             .fields()
             .requiredBoolean("success").requiredString("message")
-            .name("content").type(Schema.createArray(EnrichmentEntry.SCHEMA$))
-            .noDefault().endRecord();
+            .name("content").type(Schema.createArray(EnrichmentEntry.SCHEMA$)).noDefault()
+            .name("headers").type(Schema.createMap(Schema.create(Schema.Type.STRING))).withDefault(Collections.emptyMap())
+            .endRecord();
     @Override
     public Schema getSchema() {
         return SCHEMA$;
