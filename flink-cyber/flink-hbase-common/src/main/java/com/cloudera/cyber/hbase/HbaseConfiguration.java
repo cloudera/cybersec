3a8a4d9921e2ad1b26d9bdb293f5842db4d64402
vpavlenko-cloudera <vladyslav.pavlenko@cloudera.com>
2022-10-29 17:00:57 +0300

    [CYB-82] hbase configuration looks for hbase-site.xml, core-site.xml and hbase-site.xml in local directory before /etc/hbase/conf (#59)

--- flink-cyber/flink-hbase-common/src/main/java/com/cloudera/cyber/hbase/HbaseConfiguration.java
+++ flink-cyber/flink-hbase-common/src/main/java/com/cloudera/cyber/hbase/HbaseConfiguration.java
@@ -1,41 +1,45 @@
 package com.cloudera.cyber.hbase;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 
 import java.io.File;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 @Slf4j
 public class HbaseConfiguration {
+    public static final String HBASE_SITE = "hbase-site.xml";
+    public static final String CORE_SITE = "core-site.xml";
+    public static final String HDFS_SITE = "hdfs-site.xml";
 
     public static final String HBASE_CONFIG_NAME = "hbase-config";
 
-    public static org.apache.hadoop.conf.Configuration configureHbase() {
-        org.apache.hadoop.conf.Configuration hbaseClientConf = HBaseConfiguration.create();
-        String hbaseConfDir = "/etc/hbase/conf";
-        if ((new File(hbaseConfDir)).exists()) {
-            String coreSite = hbaseConfDir + "/core-site.xml";
-            String hdfsSite = hbaseConfDir + "/hdfs-site.xml";
-            String hbaseSite = hbaseConfDir + "/hbase-site.xml";
-            if ((new File(coreSite)).exists()) {
-                hbaseClientConf.addResource(new Path(coreSite));
-                log.info("Adding " + coreSite + " to hbase configuration");
-            }
+    private static final String DEFAULT_CONF_DIR = "/etc/hbase/conf/";
 
-            if ((new File(hdfsSite)).exists()) {
-                hbaseClientConf.addResource(new Path(hdfsSite));
-                log.info("Adding " + hdfsSite + " to hbase configuration");
-            }
-
-            if ((new File(hbaseSite)).exists()) {
-                hbaseClientConf.addResource(new Path(hbaseSite));
-                log.info("Adding " + hbaseSite + " to hbase configuration");
-            }
-        } else {
-            log.warn("HBase config directory '{}' not found, cannot load HBase configuration.", hbaseConfDir);
-        }
-
+    public static Configuration configureHbase() {
+        log.info("HbaseConfiguration configure hbase start");
+        Configuration hbaseClientConf = HBaseConfiguration.create();
+        Stream.of(HBASE_SITE, CORE_SITE, HDFS_SITE).forEach(confName -> {
+            addResource(hbaseClientConf, confName);
+        });
+        log.info("HbaseConfiguration configure hbase end");
         return hbaseClientConf;
+    }
+
+    private static void addResource(Configuration hbaseClientConf, String configXml) {
+        if (Objects.nonNull(hbaseClientConf.getResource(configXml))) {
+            log.warn("Using local {}", configXml);
+        } else {
+            String fullPath = HBASE_CONFIG_NAME + configXml;
+            if ((new File(fullPath)).exists()) {
+                hbaseClientConf.addResource(new Path(fullPath));
+                log.info("Adding {} to hbase configuration", fullPath);
+            } else {
+                log.warn("HBase config directory '{}' not found, cannot load HBase configuration.", DEFAULT_CONF_DIR);
+            }
+        }
     }
 }
