1282ea5eb56414efd7127c4e156b61dcd0660e6c
cduby <cduby@cloudera.com>
2022-11-11 13:57:31 -0500

    [CYB-105] add developer and deployment scripts to parcel (#61)

---
+++ flink-cyber/flink-commands/json-commands/src/main/java/com/cloudera/cyber/json/ReplaceJsonProperties.java
@@ -0,0 +1,55 @@
+package com.cloudera.cyber.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Properties;
+
+@Slf4j
+public class ReplaceJsonProperties {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            log.error("Requires <original_json_file> <property_file> <new_json_file> parameters.");
+            System.exit(1);
+        }
+
+        String templateRestConfigPath = args[0];
+        String propertyFile = args[1];
+        String generatedRestConfigPath = args[2];
+
+        Properties restProperties = new Properties();
+        try (InputStream restPropertyStream = new FileInputStream(propertyFile)) {
+            restProperties.load(restPropertyStream);
+            injectEnvironmentProperties(templateRestConfigPath, generatedRestConfigPath, restProperties);
+        }
+    }
+
+    private static void injectEnvironmentProperties(String templateRestConfigPath, String generatedRestConfigPath, Properties restProperties) throws IOException {
+
+        byte[] templateBytes = Files.readAllBytes(Paths.get(templateRestConfigPath));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(templateBytes);
+        for (Iterator<JsonNode> rootIter = rootNode.elements(); rootIter.hasNext(); ) {
+            JsonNode nextConfig = rootIter.next();
+            JsonNode propertiesNode = nextConfig.get("properties");
+            if (propertiesNode != null && propertiesNode.isObject()) {
+                ObjectNode propertiesObject = (ObjectNode)propertiesNode;
+                for (String propertyName : restProperties.stringPropertyNames()) {
+                    if (propertiesObject.has(propertyName)) {
+                        propertiesObject.put(propertyName, restProperties.getProperty(propertyName));
+                    }
+                }
+            }
+        }
+
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(generatedRestConfigPath), rootNode);
+    }
+}
