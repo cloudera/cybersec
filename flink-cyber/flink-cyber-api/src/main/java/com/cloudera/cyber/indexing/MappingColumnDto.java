adf9f6f2af0d5be252af2db08afca1e92a96749d
Stas Panasiuk <stas.panasiuk@cloudera.com>
2023-03-20 10:46:22 +0100

    CYB-4 indexing DTOs

---
+++ flink-cyber/flink-cyber-api/src/main/java/com/cloudera/cyber/indexing/MappingColumnDto.java
@@ -0,0 +1,54 @@
+package com.cloudera.cyber.indexing;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MappingColumnDto {
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("kafka_name")
+    private String kafkaName;
+
+    @JsonProperty("path")
+    private String path;
+
+    @JsonProperty("transformation")
+    private String transformation;
+
+    @JsonProperty("is_map")
+    private Boolean isMap;
+
+    public String getKafkaName() {
+        final String properName = kafkaName == null ? name : kafkaName;
+        if (getIsMap()) {
+            return String.format("['%s']", properName);
+        } else {
+            if (getPath().equals("..")) {
+                return String.format("`%s`", properName);
+            }
+            return String.format(".`%s`", properName);
+        }
+    }
+
+    public String getRawKafkaName(){
+        return kafkaName;
+    }
+
+    public String getPath() {
+        return StringUtils.hasText(path)
+                ? (path.equals(".") ? "" : path)
+                : "extensions";
+    }
+
+    public Boolean getIsMap() {
+        return isMap == null ? path == null : isMap;
+    }
+}
