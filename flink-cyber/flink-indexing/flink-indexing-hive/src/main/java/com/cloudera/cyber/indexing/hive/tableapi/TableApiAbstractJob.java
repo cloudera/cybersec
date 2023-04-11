4181e7227ae19428a05841698e0a1fbe69612fd0
Stas Panasiuk <stas.panasiuk@cloudera.com>
2023-04-11 08:39:02 -0400

    CYB-140 abstraction improved

--- flink-cyber/flink-indexing/flink-indexing-hive/src/main/java/com/cloudera/cyber/indexing/hive/tableapi/TableApiAbstractJob.java
+++ flink-cyber/flink-indexing/flink-indexing-hive/src/main/java/com/cloudera/cyber/indexing/hive/tableapi/TableApiAbstractJob.java
@@ -1,10 +1,13 @@
 package com.cloudera.cyber.indexing.hive.tableapi;
 
+import com.cloudera.cyber.flink.Utils;
+import com.cloudera.cyber.indexing.MappingColumnDto;
 import com.cloudera.cyber.indexing.MappingDto;
 import com.cloudera.cyber.indexing.TableColumnDto;
 import com.cloudera.cyber.indexing.hive.FilterMapFunction;
 import com.cloudera.cyber.scoring.ScoredMessage;
-import groovy.util.logging.Slf4j;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.collect.Streams;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -13,32 +16,51 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.SqlDialect;
 import org.apache.flink.table.api.bridge.java.StreamStatementSet;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
-import org.apache.flink.table.catalog.hive.HiveCatalog;
 import org.apache.flink.types.Row;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-@Slf4j
+
 public abstract class TableApiAbstractJob {
 
+    private static final String TABLES_INIT_FILE_PARAM = "flink.tables-init-file";
+    private static final String MAPPING_FILE_PARAM = "flink.mapping-file";
     protected static final String KAFKA_TABLE = "KafkaTempView";
 
+    protected static final String BASE_COLUMN_MAPPING_JSON = "base-column-mapping.json";
+
+    private final List<MappingColumnDto> defaultMappingList;
+    private final List<TableColumnDto> defaultColumnList;
     protected final DataStream<ScoredMessage> source;
     protected final StreamExecutionEnvironment env;
     protected final ParameterTool params;
+    protected final String connectorName;
 
-    public TableApiAbstractJob(ParameterTool params, StreamExecutionEnvironment env, DataStream<ScoredMessage> source) {
+    public TableApiAbstractJob(ParameterTool params, StreamExecutionEnvironment env, DataStream<ScoredMessage> source,
+                               String connectorName, String baseTableJson) throws IOException {
         this.params = params;
         this.env = env;
         this.source = source;
+        this.connectorName = connectorName;
+        defaultMappingList = Utils.readResourceFile(BASE_COLUMN_MAPPING_JSON, getClass(),
+                new TypeReference<List<MappingColumnDto>>() {
+                });
+        defaultColumnList = Utils.readResourceFile(baseTableJson, getClass(),
+                new TypeReference<List<TableColumnDto>>() {
+                });
     }
 
     public void startJob() throws Exception {
@@ -48,8 +70,8 @@ public abstract class TableApiAbstractJob {
         System.out.println("Configuring Table env...");
         configure(tableEnv);
 
-        System.out.printf("Registering Hive catalog... %s%n", String.join(", ", tableEnv.listCatalogs()));
-        registerHiveCatalog(tableEnv);
+        System.out.printf("Registering %s catalog... %s%n", connectorName, String.join(", ", tableEnv.listCatalogs()));
+        registerCatalog(tableEnv);
 
         System.out.println("Getting tables config...");
         final Map<String, List<TableColumnDto>> tablesConfig = getTablesConfig();
@@ -76,7 +98,7 @@ public abstract class TableApiAbstractJob {
             final String insertSql = buildInsertSql(topic, mappingDto);
             try {
                 insertStatementSet.addInsertSql(insertSql);
-                System.out.printf("Insert SQL added to the queue for the table: %s%nSQL: %s%n", mappingDto.getHiveTable(), insertSql);
+                System.out.printf("Insert SQL added to the queue for the table: %s%nSQL: %s%n", mappingDto.getTableName(), insertSql);
             } catch (Exception e) {
                 System.err.printf("Error adding insert to the statement set: %s%n", insertSql);
                 throw e;
@@ -85,21 +107,38 @@ public abstract class TableApiAbstractJob {
 
         System.out.println("Executing Insert statement list...");
         insertStatementSet.execute();
-
-        System.out.println("TableApiHiveJob is done!");
+        System.out.println("TableApiJob is done!");
     }
 
-    protected abstract Map<String, List<TableColumnDto>> getTablesConfig() throws IOException;
+    protected abstract String buildTableDLL(String tableName, List<TableColumnDto> columnList);
 
-    protected void setConnectorDialect(StreamTableEnvironment tableEnv){
+    protected abstract void registerCatalog(StreamTableEnvironment tableEnv);
+
+    protected void setConnectorDialect(StreamTableEnvironment tableEnv) {
         //to be overwritten if needed
     }
 
-    protected abstract void createTableIfNotExists(StreamTableEnvironment tableEnv, Set<String> tableList, String tableName, List<TableColumnDto> columnList);
+    protected void createTableIfNotExists(StreamTableEnvironment tableEnv, Set<String> tableList, String tableName, List<TableColumnDto> columnList) {
+        if (tableList.contains(tableName)) {
+            System.out.printf("%s table [%s] already exists. Skipping its creation.%n", connectorName, tableName);
+        } else {
+            System.out.printf("Creating %s table %s...%n", connectorName, tableName);
+            final String ddl = buildTableDLL(tableName, columnList);
+            try {
+                tableEnv.executeSql(ddl);
+            } catch (Exception e) {
+                System.err.printf("Error executing the %s DDL: %s%n", connectorName, ddl);
+                throw e;
+            }
+            System.out.printf("%s table created: %s%n", connectorName, tableName);
+        }
+    }
 
-    protected abstract Map<String, MappingDto> getTopicMapping() throws IOException;
-
-    protected abstract String getInsertColumns(MappingDto mappingDto);
+    protected String getInsertColumns(MappingDto mappingDto) {
+        return mappingDto.getColumnMapping().stream()
+                .map(MappingColumnDto::getName)
+                .collect(Collectors.joining("`, `", " `", "` "));
+    }
 
     private StreamTableEnvironment getTableEnvironment() {
         return StreamTableEnvironment.create(env);
@@ -112,25 +151,15 @@ public abstract class TableApiAbstractJob {
         tableEnv.executeSql("DESCRIBE " + KAFKA_TABLE).print();
     }
 
-    private void registerHiveCatalog(StreamTableEnvironment tableEnv) {
-        String name = "hive";
-        String defaultDatabase = params.get("hive.dbname", "cyber");
-        String hiveConfDir = params.get("hive.confdir", "/etc/hive/conf");
-
-        HiveCatalog hive = new HiveCatalog(name, defaultDatabase, hiveConfDir);
-        tableEnv.registerCatalog(name, hive);
-        tableEnv.useCatalog(name);
-    }
-
     private void configure(StreamTableEnvironment tableEnv) {
         final HashMap<String, String> conf = new HashMap<>();
-        conf.put("pipeline.name", params.get("flink.job.name", "Indexing - Hive TableApi"));
+        conf.put("pipeline.name", params.get("flink.job.name", String.format("Indexing - %s TableApi", connectorName)));
         tableEnv.getConfig().addConfiguration(Configuration.fromMap(conf));
         tableEnv.createTemporarySystemFunction("filterMap", FilterMapFunction.class);
     }
 
     private String buildInsertSql(String topic, MappingDto mappingDto) {
-        return String.join("\n", "insert into " + mappingDto.getHiveTable() + "(" + getInsertColumns(mappingDto) + ")",
+        return String.join("\n", "insert into " + mappingDto.getTableName() + "(" + getInsertColumns(mappingDto) + ")",
                 " SELECT " + getKafkaFromColumns(mappingDto),
                 " from " + KAFKA_TABLE,
                 String.format(" where `message`.`originalSource`.`topic`='%s'", topic));
@@ -162,6 +191,54 @@ public abstract class TableApiAbstractJob {
                             : fullPath;
                 })
                 .collect(Collectors.joining(", ", " ", " "));
+    }
+
+    protected Map<String, MappingDto> getTopicMapping() throws IOException {
+        TypeReference<HashMap<String, MappingDto>> typeRef
+                = new TypeReference<HashMap<String, MappingDto>>() {
+        };
+        final HashMap<String, MappingDto> columnMappingMap = Utils.readFile(params.getRequired(MAPPING_FILE_PARAM), typeRef);
+
+        //adding the default column mappings to each topic
+        columnMappingMap.values().forEach(mapping -> {
+            final List<MappingColumnDto> customMappings = Optional.ofNullable(mapping.getColumnMapping())
+                    .orElse(Collections.emptyList());
+            final Collection<MappingColumnDto> columnMapping = Streams.concat(
+                            customMappings.stream(),
+                            defaultMappingList.stream())
+                    .collect(Collectors.toMap(MappingColumnDto::getName, Function.identity(), (f, s) -> f)).values();
+
+            mapping.setColumnMapping(new ArrayList<>(columnMapping));
+        });
+        return columnMappingMap;
+    }
+
+    protected Map<String, List<TableColumnDto>> getTablesConfig() throws IOException {
+        TypeReference<HashMap<String, List<TableColumnDto>>> typeRef
+                = new TypeReference<HashMap<String, List<TableColumnDto>>>() {
+        };
+        final String filePath = params.get(TABLES_INIT_FILE_PARAM);
+        if (filePath == null) {
+            return Collections.emptyMap();
+        }
+        final HashMap<String, List<TableColumnDto>> columnMap = Utils.readFile(filePath, typeRef);
+        //adding the default columns to each table
+        columnMap.forEach((tableName, columnList) -> {
+            final List<TableColumnDto> customColumns = Optional.ofNullable(columnList)
+                    .orElse(Collections.emptyList());
+            final Collection<TableColumnDto> combinedColumns = Streams.concat(customColumns.stream(), defaultColumnList.stream())
+                    .collect(Collectors.toMap(TableColumnDto::getName, Function.identity(), (f, s) -> f)).values();
+            columnMap.put(tableName, new ArrayList<>(combinedColumns));
+        });
+        return columnMap;
+    }
+
+    protected String getColumnList(List<TableColumnDto> columnList) {
+        return columnList.stream()
+                .map(col -> String.format("`%s` %s %s",
+                        col.getName(), col.getType(),
+                        Optional.ofNullable(col.getNullable()).orElse(true) ? "" : "NOT NULL"))
+                .collect(Collectors.joining(", \n", " ", " \n"));
     }
 
 }
