209dafb64db912d511f08fcd21ae91b02d683624
Vladyslav Pavlenko <30902180+vpavlenko-cv@users.noreply.github.com>
2023-04-17 22:30:39 +0300

    [CYB-44] profile validation should check uniqueness of profile name and measurements (#8)

---
+++ flink-cyber/flink-profiler-java/src/main/java/com/cloudera/cyber/profiler/ProfileUtils.java
@@ -0,0 +1,40 @@
+package com.cloudera.cyber.profiler;
+
+import com.cloudera.cyber.flink.Utils;
+import com.cloudera.cyber.profiler.dto.MeasurementDto;
+import com.cloudera.cyber.profiler.dto.ProfileDto;
+import java.util.Objects;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ProfileUtils {
+
+    public static boolean compareProfileDto(ProfileDto profileDto1, ProfileDto profileDto2) {
+        if (profileDto1 == null || profileDto2 == null) {
+            return false;
+        }
+        if (profileDto1 == profileDto2) {
+            return true;
+        }
+
+        return Objects.equals(profileDto1.getProfileGroupName(), profileDto2.getProfileGroupName()) && Objects.equals(profileDto1.getKeyFieldNames(), profileDto2.getKeyFieldNames())
+            && Utils.isTimeEqual(profileDto1.getPeriodDuration(), profileDto1.getPeriodDurationUnit(), profileDto2.getPeriodDuration(), profileDto2.getPeriodDurationUnit())
+            && Utils.isTimeEqual(profileDto1.getStatsSlide(), profileDto1.getStatsSlideUnit(),
+            profileDto2.getStatsSlide(), profileDto2.getStatsSlideUnit());
+    }
+
+
+    public static boolean compareMeasurementDto(MeasurementDto measurementDto1, MeasurementDto measurementDto2) {
+        if (measurementDto1 == null || measurementDto2 == null) {
+            return false;
+        }
+        if (measurementDto1 == measurementDto2) {
+            return true;
+        }
+        return Objects.equals(measurementDto1.getFieldName(), measurementDto2.getFieldName()) && Objects.equals(measurementDto1.getResultExtensionName(), measurementDto2.getResultExtensionName()) && Objects.equals(
+            measurementDto1.getAggregationMethod(), measurementDto2.getAggregationMethod()) && Objects.equals(measurementDto1.getFormat(),
+            measurementDto2.getFormat()) && Utils.isTimeEqual(measurementDto1.getFirstSeenExpirationDuration(), measurementDto1.getFirstSeenExpirationDurationUnit(), measurementDto2.getFirstSeenExpirationDuration(),
+            measurementDto2.getFirstSeenExpirationDurationUnit());
+    }
+
+}
