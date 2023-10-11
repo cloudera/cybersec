f08a228dc15675bbbbdb8a061ad9a28a0e3a007c
Stas Panasiuk <stas.panasiuk@cloudera.com>
2023-10-11 11:24:54 -0400

    CYB-151: Management part sending config is done except the UI and pipelineDir

--- flink-cyber/metron-parser-chain/parser-chains-config-service/src/main/java/com/cloudera/parserchains/queryservice/controller/JobController.java
+++ flink-cyber/metron-parser-chain/parser-chains-config-service/src/main/java/com/cloudera/parserchains/queryservice/controller/JobController.java
@@ -1,11 +1,10 @@
 package com.cloudera.parserchains.queryservice.controller;
 
-import com.cloudera.parserchains.core.model.define.ParserChainSchema;
 import com.cloudera.parserchains.queryservice.common.ApplicationConstants;
 import com.cloudera.parserchains.queryservice.common.exception.FailedAllClusterReponseException;
 import com.cloudera.parserchains.queryservice.common.exception.FailedClusterReponseException;
 import com.cloudera.parserchains.queryservice.model.enums.JobActions;
-import com.cloudera.parserchains.queryservice.service.ClusterService;
+import com.cloudera.parserchains.queryservice.service.JobService;
 import com.cloudera.service.common.response.ResponseBody;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -16,13 +15,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 /**
@@ -33,6 +35,7 @@ import java.util.List;
 @RequestMapping(value = ApplicationConstants.API_BASE_URL + ApplicationConstants.API_JOBS)
 public class JobController {
 
+    private final JobService jobService;
 
     @ApiOperation(value = "Retrieves information about all cluster services.")
     @ApiResponses(value = {
@@ -46,6 +49,31 @@ public class JobController {
                                       @Schema(name = "action", description = "Jobs action for start stop restart Job", allowableValues = {JobActions.Constants.START_VALUE, JobActions.Constants.RESTART_VALUE, JobActions.Constants.STOP_VALUE, JobActions.Constants.STATUS_VALUE, JobActions.Constants.GET_CONFIG_VALUE, JobActions.Constants.UPDATE_CONFIG_VALUE}, required = true)
                                       @PathVariable("action") String action) throws FailedAllClusterReponseException {
         return null;
+    }
+
+    @PostMapping("/config/{clusterId}/{jobIdHex}")
+    public ResponseBody updateJobConfig(@ApiParam(name = "clusterId", value = "The ID of the cluster to update config on.", required = true)
+                                        @PathVariable("clusterId") String clusterId,
+                                        @Schema(name = "jobIdHex", description = "Job ID to update config of", allowableValues = {JobActions.Constants.START_VALUE, JobActions.Constants.RESTART_VALUE, JobActions.Constants.STOP_VALUE, JobActions.Constants.STATUS_VALUE, JobActions.Constants.GET_CONFIG_VALUE, JobActions.Constants.UPDATE_CONFIG_VALUE}, required = true)
+                                        @PathVariable("jobIdHex") String jobIdHex,
+                                        @RequestParam("config") MultipartFile config) throws FailedClusterReponseException, IOException {
+        final String filename = config.getOriginalFilename();
+        if (filename == null || !filename.endsWith(".tar.gz")){
+            throw new RuntimeException("You should provide config as a .tar.gz archive");
+        }
+        //Kafka message body should be less than 1Mb, but we should fit a base64 encoded file + other JSON fields.
+        if (Math.ceil(config.getSize() / 3d) * 4 >= 1000000){
+            throw new RuntimeException("Provided file should be less than ~750Kb in size.");
+        }
+
+        final com.cloudera.service.common.request.RequestBody requestBody =
+                com.cloudera.service.common.request.RequestBody.builder()
+//                        .clusterServiceId() FIXME what it's for and should we set it?
+                        .jobIdHex(jobIdHex)
+//                        .pipelineDir() FIXME get from jobIdHex?
+                        .payload(Base64.getEncoder().encode(config.getBytes()))
+                        .build();
+        return jobService.makeRequest(clusterId, requestBody, JobActions.Constants.UPDATE_CONFIG_VALUE);
     }
 
 
