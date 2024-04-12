a57f6a065b558a1bc58bbedd830512c9d03c31bb
stas-panasiuk <112094458+stas-panasiuk@users.noreply.github.com>
2024-04-12 08:57:20 -0400

    [CYB-164] Pipeline CRUD + Inter-cluster communication (#41)

---
+++ flink-cyber/metron-parser-chain/parser-chains-config-service/src/main/java/com/cloudera/parserchains/queryservice/config/kafka/KafkaConfig.java
@@ -0,0 +1,102 @@
+package com.cloudera.parserchains.queryservice.config.kafka;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.GenericMessageListenerContainer;
+
+@Slf4j
+@EnableKafka
+@Configuration
+@RequiredArgsConstructor
+public class KafkaConfig {
+
+  /***
+   * Provides the default kafka properties for the consumers
+   * @return Default kafka properties for the consumers
+   */
+  @Bean
+  @Primary
+  public ClouderaKafkaProperties kafkaProperties() {
+    return new ClouderaKafkaProperties();
+  }
+
+  /***
+   * Provides a map with key=clusterId and value=ClouderaKafkaProperties
+   * @return Map with key of clusterId and value - kafkaProperties. This map is a mapping between clusterId and connection details for that cluster
+   */
+  @Bean(name = "kafka-external-cluster-map")
+  @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+  @ConfigurationProperties("spring.kafka.external-clusters")
+  public Map<String, ClouderaKafkaProperties> replyKafkaPropertiesMap() {
+    return new HashMap<>();
+  }
+
+  /**
+   * Map of KafkaTemplates that with key=clusterId, which allows to send requests over Kafka to different clusters and
+   * get responses.
+   *
+   * @param replyKafkaPropertiesMap - replyKafkaPropertiesMap bean with properties for each cluster, injected by Spring
+   * @return Map with key of clusterId and value - kafkaTemplate that allows to send requests over Kafka to different clusters and get
+   * responses.
+   */
+  @Bean(name = "kafkaTemplatePool")
+  @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+  public Map<String, ClouderaReplyingKafkaTemplate<String, String, String>> kafkaTemplatePool(
+      @Qualifier("kafka-external-cluster-map") Map<String, ClouderaKafkaProperties> replyKafkaPropertiesMap) {
+    final Map<String, ClouderaReplyingKafkaTemplate<String, String, String>> templatePool = new HashMap<>();
+
+    replyKafkaPropertiesMap.forEach((clusterId, kafkaProperties) -> {
+      final ProducerFactory<String, String> producerFactory = producerFactory(kafkaProperties);
+      final ConsumerFactory<String, String> consumerFactory = consumerFactory(kafkaProperties);
+      final GenericMessageListenerContainer<String, String> replyContainer = replyContainer(consumerFactory,
+          kafkaProperties.getReplyTopic());
+
+      final ClouderaReplyingKafkaTemplate<String, String, String> kafkaTemplate = replyingKafkaTemplate(producerFactory,
+          replyContainer, kafkaProperties.getRequestTopic());
+      kafkaTemplate.start();
+
+      templatePool.put(clusterId, kafkaTemplate);
+    });
+
+    return Collections.unmodifiableMap(templatePool);
+  }
+
+  private static ProducerFactory<String, String> producerFactory(KafkaProperties kafkaProperties) {
+    return new DefaultKafkaProducerFactory<>(kafkaProperties.buildProducerProperties());
+  }
+
+  private static ConsumerFactory<String, String> consumerFactory(KafkaProperties kafkaProperties) {
+    return new DefaultKafkaConsumerFactory<>(kafkaProperties.buildConsumerProperties());
+  }
+
+  private static ClouderaReplyingKafkaTemplate<String, String, String> replyingKafkaTemplate(
+      ProducerFactory<String, String> producerFactory, GenericMessageListenerContainer<String, String> replyContainer,
+      String requestTopic) {
+    return new ClouderaReplyingKafkaTemplate<>(producerFactory, replyContainer, requestTopic);
+  }
+
+  private static GenericMessageListenerContainer<String, String> replyContainer(
+      ConsumerFactory<String, String> consumerFactory, String replyTopic) {
+    ContainerProperties containerProperties = new ContainerProperties(replyTopic);
+    return new ConcurrentMessageListenerContainer<>(consumerFactory, containerProperties);
+  }
+
+}
---
+++ flink-cyber/metron-parser-chain/parser-chains-config-service/src/main/java/com/cloudera/parserchains/queryservice/service/KafkaService.java
@@ -0,0 +1,84 @@
+package com.cloudera.parserchains.queryservice.service;
+
+import static com.cloudera.parserchains.queryservice.common.ApplicationConstants.BODY_PARAM;
+import static com.cloudera.parserchains.queryservice.common.ApplicationConstants.HEADERS_PARAM;
+import static com.cloudera.parserchains.queryservice.common.ApplicationConstants.STATUS_PARAM;
+import com.cloudera.parserchains.core.utils.JSONUtils;
+import com.cloudera.parserchains.queryservice.config.kafka.ClouderaReplyingKafkaTemplate;
+import com.cloudera.parserchains.queryservice.model.enums.KafkaMessageType;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.KafkaException;
+import org.springframework.kafka.requestreply.KafkaReplyTimeoutException;
+import org.springframework.kafka.requestreply.RequestReplyFuture;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaService {
+
+  private static final ObjectMapper MAPPER = JSONUtils.INSTANCE.getMapper();
+  private static final ObjectReader HEADERS_READER = MAPPER.readerFor(HttpHeaders.class);
+
+  @Qualifier("kafkaTemplatePool")
+  private final Map<String, ClouderaReplyingKafkaTemplate<String, String, String>> kafkaTemplatePool;
+
+
+  public <T> ResponseEntity<T> sendWithReply(KafkaMessageType messageType, String clusterId, Object body,
+      ObjectReader responseReader)
+      throws JsonProcessingException {
+    final ClouderaReplyingKafkaTemplate<String, String, String> kafkaTemplate = kafkaTemplatePool.get(clusterId);
+    if (kafkaTemplate == null) {
+      throw new RuntimeException("Cluster not found!");
+    }
+
+    final String jsonBody = JSONUtils.INSTANCE.toJSON(body, false);
+
+    ProducerRecord<String, String> record = new ProducerRecord<>(kafkaTemplate.getRequestTopic(),
+        messageType.name(), jsonBody);
+    RequestReplyFuture<String, String, String> replyFuture = kafkaTemplate.sendAndReceive(record);
+    try {
+      ConsumerRecord<String, String> consumerRecord = replyFuture.get(15, TimeUnit.SECONDS);
+      if (consumerRecord == null) {
+        throw new KafkaException("Got no reply");
+      }
+      final String stringReply = consumerRecord.value();
+
+      return deserializeReply(stringReply, responseReader);
+    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+      // Handle exception
+      throw new KafkaReplyTimeoutException("Timeout while waiting for reply");
+    } catch (IOException e) {
+      throw new RuntimeException("Wasn't able to deserialize the response!", e);
+    }
+  }
+
+  private static <T> ResponseEntity<T> deserializeReply(String reply, ObjectReader responseReader) throws IOException {
+    final JsonNode replyBody = MAPPER.readTree(reply);
+
+    final T body = responseReader == null
+        ? null
+        : responseReader.readValue(replyBody.get(BODY_PARAM));
+    final HttpHeaders headers = HEADERS_READER.readValue(replyBody.get(HEADERS_PARAM));
+    final HttpStatus status = HttpStatus.valueOf(replyBody.get(STATUS_PARAM).asText());
+
+    return new ResponseEntity<>(body, headers, status);
+  }
+
+}
