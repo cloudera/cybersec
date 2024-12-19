8767f5dcd1e2099614e89357fe8602789c2bff83
vladyslav.pavlenko <vladyslav.pavlenko@cloudera.com>
2024-12-19 16:15:19 +0200

    [CYB - 195][UI] Parser job configuration.
    * style width.

--- flink-cyber/metron-parser-chain/parser-chains-config-service/frontend/parser-chains-client/src/app/cluster/pipelines/pipeline-stepper/pipeline-stepper.component.spec.ts
+++ flink-cyber/metron-parser-chain/parser-chains-config-service/frontend/parser-chains-client/src/app/cluster/pipelines/pipeline-stepper/pipeline-stepper.component.spec.ts
@@ -1,7 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PipelineStepperComponent} from './pipeline-stepper.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SharedModule} from 'src/app/shared/share.module';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
@@ -16,10 +16,26 @@ import {AsyncPipe, NgComponentOutlet, NgForOf, NgIf, NgTemplateOutlet} from '@an
 import {MatSelectModule} from '@angular/material/select';
 import {MatStepperModule} from '@angular/material/stepper';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {PipelineService} from 'src/app/services/pipeline.service';
+import {of} from 'rxjs';
+import {ChainListPageService} from 'src/app/services/chain-list-page.service';
+import {ChainModel} from 'src/app/chain-list-page/chain.model';
+import {Router} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+const mockPipeline = ['foo-pipeline1', 'foo-pipeline2'];
+const mockChains: ChainModel[] = [{id: '1', name: 'test1'}, {id: '2', name: 'test2'}];
 
 describe('PipelineStepperComponent', () => {
   let component: PipelineStepperComponent;
   let fixture: ComponentFixture<PipelineStepperComponent>;
+  let pipelineService: jasmine.SpyObj<PipelineService>;
+  let chainListPageService: jasmine.SpyObj<ChainListPageService>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({      imports: [
@@ -41,13 +57,43 @@ describe('PipelineStepperComponent', () => {
         FormsModule,
         NgForOf,
         MatStepperModule,
+        RouterTestingModule.withRoutes([]),
+        MatTabsModule,
+        ReactiveFormsModule,
+        MatSlideToggleModule,
+        MatChipsModule,
+        MatAutocompleteModule,
         NoopAnimationsModule
+      ],
+      providers: [
+        {
+          provide: PipelineService,
+          useValue: jasmine.createSpyObj('PipelineService', ['getPipelines', 'createPipeline', 'renamePipeline', 'deletePipeline'])
+        },
+        {
+          provide: ChainListPageService,
+          useValue: jasmine.createSpyObj('ChainListPageService', ['createChain', 'getChains', 'deleteChain', 'getPipelines'])
+        },
       ],
 
       declarations: [ PipelineStepperComponent ]
     })
     .compileComponents();
 
+    pipelineService = TestBed.inject(PipelineService) as jasmine.SpyObj<PipelineService>;
+    pipelineService.getPipelines.and.returnValue(of(mockPipeline));
+    chainListPageService = TestBed.inject(ChainListPageService) as jasmine.SpyObj<ChainListPageService>;
+    chainListPageService.getChains.and.returnValue(of(mockChains));
+
+    router = TestBed.inject(Router);
+    spyOn(router, 'getCurrentNavigation').and.returnValue({
+      extras: {state: {data: {clusterId: 'test1', pipelineName: 'foo-pipe', branch: 'testbranch'}}},
+      id: 0,
+      initialUrl: null,
+      extractedUrl: null,
+      trigger: null,
+      previousNavigation: null
+    })
     fixture = TestBed.createComponent(PipelineStepperComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
