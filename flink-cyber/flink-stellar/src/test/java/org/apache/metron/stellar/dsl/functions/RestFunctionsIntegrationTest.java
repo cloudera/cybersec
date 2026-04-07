78e056a9e71d02bdac49089e591d1092712a8f9a
carolynduby <carolynduby@ieee.org>
2026-04-07 12:11:41 -0400

    [CYB-227] Upgrade to CSA 1.16.1.0 and CDP 7.3.1.706-4 (#111)

--- flink-cyber/flink-stellar/src/test/java/org/apache/metron/stellar/dsl/functions/RestFunctionsIntegrationTest.java
+++ flink-cyber/flink-stellar/src/test/java/org/apache/metron/stellar/dsl/functions/RestFunctionsIntegrationTest.java
@@ -6,9 +6,7 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,19 +17,13 @@
 package org.apache.metron.stellar.dsl.functions;
 
 import com.google.common.collect.ImmutableMap;
-import org.apache.commons.io.FileUtils;
 import org.apache.metron.stellar.dsl.Context;
 import org.apache.metron.stellar.dsl.ParseException;
-import org.junit.Rule;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport;
-import org.junit.rules.TemporaryFolder;
 import org.mockserver.integration.ClientAndServer;
+import org.mockserver.socket.PortFactory;
 
-import java.io.File;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -47,47 +39,34 @@ import static org.mockserver.model.HttpResponse.response;
 
 @EnableRuleMigrationSupport
 public class RestFunctionsIntegrationTest {
-  @Rule
-  public TemporaryFolder tempDir = new TemporaryFolder();
 
-  private static final int MOCK_PROXY_PORT = 1080;
+  private static ClientAndServer mockServerClient;
 
-  // Use plain Java API, because we can't use the @Rule and the JUnit5 extension isn't released.
-  private ClientAndServer mockServerClient;
-
-  private String baseUri;
-  private String getUri;
-  private String emptyGetUri;
-  private String postUri;
-  private String emptyPostUri;
+  private static String baseUri;
+  private static String getUri;
+  private static String emptyGetUri;
+  private static String getWithDelayUri;
+  private static String postUri;
   private Context context;
 
-  private File basicAuthPasswordFile;
-  private String basicAuthPassword = "password";
-  private File proxyBasicAuthPasswordFile;
-  private String proxyAuthPassword = "proxyPassword";
-
   @BeforeEach
-  public void setup() throws Exception {
-    mockServerClient = startClientAndServer(MOCK_PROXY_PORT);
-
+  public void testSetup() {
     context = new Context.Builder()
             .with(Context.Capabilities.GLOBAL_CONFIG, HashMap::new)
             .build();
+  }
 
-    // Store the passwords in the local file system
-    basicAuthPasswordFile = tempDir.newFile("basicAuth.txt");
-    FileUtils.writeStringToFile(basicAuthPasswordFile, basicAuthPassword, StandardCharsets.UTF_8);
-    proxyBasicAuthPasswordFile = tempDir.newFile("proxyBasicAuth.txt");
-    FileUtils.writeStringToFile(proxyBasicAuthPasswordFile, proxyAuthPassword, StandardCharsets.UTF_8);
+  @BeforeAll
+  public static void setup() throws Exception {
+    mockServerClient = startClientAndServer(PortFactory.findFreePort());
+    mockServerClient.reset();
 
     // By default, the mock server expects a GET request with the path set to /get
-    baseUri = String.format("http://localhost:%d", MOCK_PROXY_PORT);
+    baseUri = String.format("http://localhost:%d", mockServerClient.getPort());
     getUri = baseUri + "/get";
     emptyGetUri = baseUri + "/get/empty";
     postUri = baseUri + "/post";
-    emptyPostUri = baseUri + "/post/empty";
-    mockServerClient.when(
+      mockServerClient.when(
             request()
                     .withMethod("GET")
                     .withPath("/get"))
@@ -113,10 +92,19 @@ public class RestFunctionsIntegrationTest {
                     .withPath("/post/empty"))
             .respond(response()
                     .withStatusCode(404));
+
+    getWithDelayUri = baseUri + "/get_with_delay";
+    mockServerClient.when(
+                    request()
+                            .withMethod("GET")
+                            .withPath("get_with_delay"))
+            .respond(response()
+                    .withDelay(TimeUnit.MILLISECONDS, 1000)
+                    .withBody("{\"get\":\"success\"}"));
   }
 
-  @AfterEach
-  public void teardown() {
+  @AfterAll
+  public static void teardown() {
     mockServerClient.stop();
   }
 
@@ -160,19 +148,20 @@ public class RestFunctionsIntegrationTest {
   @Test
   @SuppressWarnings("unchecked")
   public void restGetShouldSucceedWithProxy() {
+    final String getWithProxyPath = "/get_with_proxy";
     mockServerClient.when(
             request()
                     .withMethod("GET")
-                    .withPath("/get"))
+                    .withPath(getWithProxyPath))
             .respond(response()
                     .withBody("{\"proxyGet\":\"success\"}"));
 
     context.addCapability(Context.Capabilities.GLOBAL_CONFIG, () -> new HashMap<String, Object>() {{
       put(PROXY_HOST, "localhost");
-      put(PROXY_PORT, MOCK_PROXY_PORT);
+      put(PROXY_PORT, mockServerClient.getPort());
     }});
 
-    Map<String, Object> actual = (Map<String, Object>) run(String.format("REST_GET('%s')", getUri), context);
+    Map<String, Object> actual = (Map<String, Object>) run(String.format("REST_GET('%s')", baseUri.concat(getWithProxyPath)), context);
 
     assertEquals(1, actual.size());
     assertEquals("success", actual.get("proxyGet"));
@@ -183,17 +172,18 @@ public class RestFunctionsIntegrationTest {
    */
   @Test
   public void restGetShouldHandleErrorStatusCode() {
+    final String getWith403 = "/get_403";
     mockServerClient.when(
             request()
                     .withMethod("GET")
-                    .withPath("/get"))
+                    .withPath(getWith403))
             .respond(response()
                     .withStatusCode(403));
 
-    assertNull(run(String.format("REST_GET('%s')", getUri), context));
+    assertNull(run(String.format("REST_GET('%s')", baseUri.concat(getWith403)), context));
   }
 
-  private String emptyContentOverride = String.join("\n",
+  private final String emptyContentOverride = String.join("\n",
         "{",
         "\"response.codes.allowed\": [200,404],",
         "\"empty.content.override\": \"function config override\"",
@@ -208,7 +198,7 @@ public class RestFunctionsIntegrationTest {
     assertEquals("function config override", run(String.format("REST_GET('%s', %s)", emptyGetUri, emptyContentOverride), context));
   }
 
-  private String errorValueOverride = String.join("\n",
+  private final String errorValueOverride = String.join("\n",
         "{",
         "\"error.value.override\": \"error message\"",
         "}"
@@ -219,14 +209,15 @@ public class RestFunctionsIntegrationTest {
    */
   @Test
   public void restGetShouldReturnErrorValueOverride() {
+    final String getErrorPath = "/get_with_error";
     mockServerClient.when(
             request()
                     .withMethod("GET")
-                    .withPath("/get"))
+                    .withPath(getErrorPath))
             .respond(response()
                     .withStatusCode(500));
 
-    Object result = run(String.format("REST_GET('%s', %s)", getUri, errorValueOverride), context);
+    Object result = run(String.format("REST_GET('%s', %s)", baseUri.concat(getErrorPath), errorValueOverride), context);
     assertEquals("error message" , result);
   }
 
@@ -236,15 +227,6 @@ public class RestFunctionsIntegrationTest {
   @Test
   @SuppressWarnings("unchecked")
   public void restGetShouldTimeout() {
-    String uri = String.format("http://localhost:%d/get", MOCK_PROXY_PORT);
-
-    mockServerClient.when(
-            request()
-                    .withMethod("GET")
-                    .withPath("/get"))
-            .respond(response()
-                    .withDelay(TimeUnit.MILLISECONDS, 1000)
-                    .withBody("{\"get\":\"success\"}"));
 
     Map<String, Object> globalConfig = new HashMap<String, Object>() {{
       put(STELLAR_REST_SETTINGS, new HashMap<String, Object>() {{
@@ -254,11 +236,11 @@ public class RestFunctionsIntegrationTest {
 
     context.addCapability(Context.Capabilities.GLOBAL_CONFIG, () -> globalConfig);
 
-    Map<String, Object> actual = (Map<String, Object>) run(String.format("REST_GET('%s')", uri), context);
+    Map<String, Object> actual = (Map<String, Object>) run(String.format("REST_GET('%s')", getWithDelayUri), context);
     assertNull(actual);
   }
 
-  private String timeoutConfig = String.join("\n",
+  private final String timeoutConfig = String.join("\n",
         "{",
         "\"timeout\": 10",
         "}"
@@ -270,16 +252,7 @@ public class RestFunctionsIntegrationTest {
   @Test
   @SuppressWarnings("unchecked")
   public void restGetShouldTimeoutWithSuppliedTimeout() {
-    String uri = String.format("http://localhost:%d/get", MOCK_PROXY_PORT);
-    mockServerClient.when(
-            request()
-                    .withMethod("GET")
-                    .withPath("/get"))
-            .respond(response()
-                    .withDelay(TimeUnit.MILLISECONDS, 1000)
-                    .withBody("{\"get\":\"success\"}"));
-
-    String expression = String.format("REST_GET('%s', %s)", uri, timeoutConfig);
+    String expression = String.format("REST_GET('%s', %s)", getWithDelayUri, timeoutConfig);
     Map<String, Object> actual = (Map<String, Object>) run(expression, context);
     assertNull(actual);
   }
@@ -422,10 +395,10 @@ public class RestFunctionsIntegrationTest {
     ParseException e = assertThrows(ParseException.class, () -> run(String.format("REST_POST('%s', 'malformed json')", postUri), context));
     assertEquals(
         String.format(
-            "Unable to parse REST_POST('http://localhost:1080/post', 'malformed json'): " +
+            "Unable to parse REST_POST('%s', 'malformed json'): " +
                     "Unable to parse: REST_POST('%s', 'malformed json') due to: POST data 'malformed json' must be properly formatted JSON.  " +
                     "Set the 'enforce.json' property to false to disable this check.",
-            postUri),
+            postUri, postUri),
         e.getMessage());
   }
 
