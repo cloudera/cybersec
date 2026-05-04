252e1dadb8f7eb259764211422d993440b9436bf
carolynduby <carolynduby@ieee.org>
2026-05-04 17:13:55 -0400

    [CYB-239] avro generator finds avro schemas relative to config baseDir (#121)

--- flink-cyber/flink-common/src/main/java/com/cloudera/cyber/generator/GeneratorConfig.java
+++ flink-cyber/flink-common/src/main/java/com/cloudera/cyber/generator/GeneratorConfig.java
@@ -1,6 +1,7 @@
 package com.cloudera.cyber.generator;
 
 import lombok.Data;
+import org.apache.flink.core.fs.Path;
 
 import java.io.IOException;
 import java.util.List;
@@ -10,9 +11,10 @@ public class GeneratorConfig {
     String baseDirectory;
     List<GenerationSource> generationSources;
 
-    public void open() throws IOException {
+    public void open(Path configPath) throws IOException {
         for(GenerationSource source : getGenerationSources()) {
-            source.readAvroSchema(baseDirectory);
+            String configDirectory = (configPath != null) ? new Path(configPath.getParent(), baseDirectory).toString() : baseDirectory;
+            source.readAvroSchema(configDirectory);
         }
     }
 }
